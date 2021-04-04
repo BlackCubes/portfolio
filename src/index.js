bbe42fc105056fb6d418800b413990fa912a9082
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-03 23:24:17 -0700

    Imported: inputController, submitController, checkErrors, and sendEmail functions. Added: contactForm element. Delegated: contact form with validation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,51 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+import { inputController, submitController } from './formControllers';
+import { checkErrors, sendEmail } from './utils';
+
+// DOM ELEMENTS
+const contactForm = document.getElementById('contactForm');
+
+// DELEGATION
+// -- contact form
+if (contactForm) {
+  const errorStack = {};
+
+  // Validate name, email, and message inputs
+  inputController(document.getElementById('name'), errorStack);
+  inputController(document.getElementById('email'), errorStack);
+  inputController(document.getElementById('message'), errorStack);
+
+  if (checkErrors(errorStack)) {
+    contactForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+
+      // Validate the form
+      submitController(
+        errorStack,
+        document.getElementById('name'),
+        document.getElementById('email'),
+        document.getElementById('message')
+      );
+
+      if (checkErrors(errorStack)) {
+        const form = new FormData();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        form.append('name', name);
+        form.append('email', email);
+        form.append('message', message);
+
+        document.getElementById('contactFormBtn').textContent = 'Submitting...';
+
+        await sendEmail(form);
+
+        document.getElementById('contactFormBtn').textContent = 'Send';
+      }
+    });
+  }
+}
