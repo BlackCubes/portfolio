71a0fdc4b3b021218c3753ae7e03d89fe700383a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-04 08:43:57 -0700

    Removed the validation for the inputs

--- src/formControllers/inputController.js
+++ src/formControllers/inputController.js
@@ -1,24 +1,12 @@
-import { checkErrors, validateForm } from '../utils';
-
-const inputController = (element, errorStack) => {
+const inputController = (element) => {
   element.addEventListener('input', (e) => {
-    const { name, value } = e.target;
+    const { value } = e.target;
 
     // This is for styling the custom placeholder
     if (value.length > 0) {
       e.target.classList.add('not-empty');
     } else {
       e.target.classList.remove('not-empty');
-    }
-
-    validateForm(name, value, errorStack);
-
-    if (checkErrors(errorStack)) {
-      e.target.classList.remove('error');
-      e.target.classList.add('success');
-    } else {
-      e.target.classList.remove('success');
-      e.target.classList.add('error');
     }
   });
 };
