69c030bd8015c6bd22a2b11cb5de283074d5bec7
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-03 23:38:05 -0700

    Refactored: stored the errors by doing it the standard way into objects

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -4,54 +4,39 @@ const validateForm = (inputName, inputValue, errors) => {
   switch (inputName) {
     case 'email':
       if (!inputValue.length) {
-        errors((err) => ({ ...err, [inputName]: 'Required.' }));
+        errors[inputName] = 'Required.';
       } else if (!regex.email.test(inputValue)) {
-        errors((err) => ({
-          ...err,
-          [inputName]: 'Please provide a valid email.',
-        }));
+        errors[inputName] = 'Please provide a valid email.';
       } else {
-        errors((err) => ({ ...err, [inputName]: '' }));
+        errors[inputName] = '';
       }
       break;
 
     case 'name':
       if (!inputValue.split(' ')[0]) {
-        errors((err) => ({ ...err, [inputName]: 'Required first name.' }));
+        errors[inputName] = 'Required first name.';
       } else if (!inputValue.split(' ')[1]) {
-        errors((err) => ({ ...err, [inputName]: 'Required last name.' }));
+        errors[inputName] = 'Required last name.';
       } else if (inputValue.length < 2) {
-        errors((err) => ({
-          ...err,
-          [inputName]: 'Must be a minimum of 2 characters.',
-        }));
+        errors[inputName] = 'Must be a minimum of 2 characters.';
       } else if (inputValue.length > 70) {
-        errors((err) => ({
-          ...err,
-          [inputName]: 'Must be less than or equal to 70 characters.',
-        }));
+        errors[inputName] = 'Must be less than or equal to 70 characters.';
       } else if (!regex.name.test(inputValue)) {
-        errors((err) => ({ ...err, [inputName]: 'Please use a valid name' }));
+        errors[inputName] = 'Please use a valid name.';
       } else {
-        errors((err) => ({ ...err, [inputName]: '' }));
+        errors[inputName] = '';
       }
       break;
 
     case 'message':
       if (!inputValue.length) {
-        errors((err) => ({ ...err, [inputName]: 'Required.' }));
+        errors[inputName] = 'Required.';
       } else if (inputValue.length < 20) {
-        errors((err) => ({
-          ...err,
-          [inputName]: 'Must be a minimum of 20 characters.',
-        }));
+        errors[inputName] = 'Must be a minimum of 20 characters.';
       } else if (inputValue.length > 280) {
-        errors((err) => ({
-          ...err,
-          [inputName]: 'Must be less than or equal to 280 characters.',
-        }));
+        errors[inputName] = 'Must be less than or equal to 280 characters.';
       } else {
-        errors((err) => ({ ...err, [inputName]: '' }));
+        errors[inputName] = '';
       }
       break;
 
