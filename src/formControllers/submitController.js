85079e84e9387193eb4afbeda9129c3c04118f47
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-04 00:22:20 -0700

    Added extra checks for the classes

--- src/formControllers/submitController.js
+++ src/formControllers/submitController.js
@@ -5,14 +5,25 @@ const submitController = (errorStack, ...elements) => {
     const { name, value } = element;
     const messageElement = element.nextElementSibling.nextElementSibling;
 
+    // Remove any potential error/success classes from previously
+    element.classList.remove('error');
+    element.classList.remove('success');
+    messageElement.classList.remove('error');
+    messageElement.classList.remove('success');
+
     validateForm(name, value, errorStack);
 
     if (checkErrors(errorStack)) {
+      messageElement.classList.remove('error');
+      messageElement.classList.add('success');
       messageElement.textContent = 'Noice!';
       element.classList.remove('error');
-      element.target.classList.add('success');
+      element.classList.add('success');
     } else {
+      element.classList.remove('success');
       element.classList.add('error');
+      messageElement.classList.remove('success');
+      messageElement.classList.add('error');
       messageElement.textContent = errorStack[name];
     }
   });
