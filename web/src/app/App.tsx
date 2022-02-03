1db249dfd4ce93bb44fa3194a6a1ae77008d39b8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-03 10:50:17 -0800

    refactor(theme-global-provider): move the theme provider and global style from index.tsx to App.tsx

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle, theme } from 'common/base';
 
 const App = () => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+  </ThemeProvider>
 );
 
 export default App;
