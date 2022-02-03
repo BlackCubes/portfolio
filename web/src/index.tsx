f9881d926cc152ec494fe62943359deab92b0e29
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-02 17:47:59 -0800

    refactor(module-imports): change some of the imports

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './app/App';
+
+import App from 'app/App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
