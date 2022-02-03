67658d61833893b0a5f1e55fe9c9a0abe28976f8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-03 10:42:54 -0800

    refactor(index-style): remove the base default style from create-react-app

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
-import './index.css';
 import App from 'app/App';
 import { GlobalStyle, theme } from 'common/base';
 
