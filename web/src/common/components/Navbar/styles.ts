5558993e119df8106edb29d183fe848003792bef
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-03 13:13:47 -0800

    feat(navbar-styles): add some stylings to the navbar

--- web/src/common/components/Navbar/styles.ts
+++ web/src/common/components/Navbar/styles.ts
@@ -2,7 +2,8 @@
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-  padding: 1rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
 `;
 
 export const Container = styled.div`
@@ -11,17 +12,39 @@ export const Container = styled.div`
 `;
 
 export const LogoWrapper = styled.div`
-  width: 5rem;
+  width: 5.5rem;
 `;
 
-export const LogoLink = styled.a``;
+export const LogoLink = styled.a`
+  display: inline-flex;
+`;
 
 export const Logo = styled.img`
   width: 100%;
 `;
 
-export const ItemsContainer = styled.ol``;
+export const ItemsContainer = styled.ul`
+  display: flex;
+  list-style: none;
+`;
 
-export const Item = styled.li``;
+export const Item = styled.li`
+  height: 100%;
+  padding-left: 4rem;
+`;
 
-export const ItemLink = styled.a``;
+export const ItemLink = styled.a`
+  display: inline-flex;
+  align-items: end;
+  height: inherit;
+  font-size: 2rem;
+  color: ${(props) => props.theme.colors.primary};
+  text-decoration: none;
+
+  &:hover {
+    color: ${(props) => props.theme.colors.secondary};
+    font-size: 1.89rem;
+    letter-spacing: 0.05rem;
+    border-bottom: 0.1rem solid ${(props) => props.theme.colors.secondary};
+  }
+`;
