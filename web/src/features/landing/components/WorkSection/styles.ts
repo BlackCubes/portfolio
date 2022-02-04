54fe6a84f8f687ee477f831bcadfe476f9715cbd
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-04 14:33:00 -0800

    feat(work-section-styles): create most of the styles for the work section

---
+++ web/src/features/landing/components/WorkSection/styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+interface IProjectLink {
+  opacity?: number;
+}
+
+export const Section = styled.section``;
+
+export const SectionTitleWrapper = styled.div`
+  border-right: 0.1rem solid ${(props) => props.theme.colors.primary};
+  opacity: 0.7;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProjectContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5rem;
+  margin-bottom: 5rem;
+`;
+
+export const ProjectTitle = styled.div``;
+
+export const ProjectDescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProjectDescription = styled.div`
+  margin-bottom: 2rem;
+`;
+
+export const ProjectLinkWrapper = styled.div``;
+
+export const ProjectLink = styled.a<IProjectLink>`
+  font-size: ${(props) => props.theme.fonts.paragraph};
+  font-weight: 700;
+  color: ${(props) => props.theme.colors.primary};
+
+  ${({ opacity }) => opacity && `opacity: ${opacity}`}
+`;
