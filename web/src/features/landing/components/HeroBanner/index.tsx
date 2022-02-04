c80abea17a223631b4b8ceeb3c4455b1711c3fe3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-04 12:30:19 -0800

    feat(hero-banner): create styles and component for the hero banner

---
+++ web/src/features/landing/components/HeroBanner/index.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from 'react';
+
+import profileImage from 'assets/img/profile-pic.jpg';
+
+import HeadingPrimary from 'common/typography/HeadingPrimary';
+import Paragraph from 'common/typography/Paragraph';
+
+import {
+  BiographyContainer,
+  Container,
+  MainBiographyWrapper,
+  ProfileImage,
+  ProfileImageWrapper,
+  SecondaryBiographyWrapper,
+} from './styles';
+
+const HeroBanner: FC = () => (
+  <Container className="default-container">
+    <ProfileImageWrapper>
+      <ProfileImage src={profileImage} alt="Profile image of Elias Gutierrez" />
+    </ProfileImageWrapper>
+
+    <BiographyContainer>
+      <MainBiographyWrapper>
+        <HeadingPrimary>
+          Hello! I&apos;m Elias Gutierrez, and I&apos;m a Software Engineer and
+          Full-Stack Developer
+        </HeadingPrimary>
+      </MainBiographyWrapper>
+
+      <SecondaryBiographyWrapper>
+        <Paragraph>
+          I enjoy architecturing with the art and mathematical model of nature
+          to create beautiful user experiences for the web, native mobile apps,
+          and data science
+        </Paragraph>
+      </SecondaryBiographyWrapper>
+    </BiographyContainer>
+  </Container>
+);
+
+export default HeroBanner;
---
+++ web/src/features/landing/components/HeroBanner/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ProfileImageWrapper = styled.div`
+  width: 7rem;
+`;
+
+export const ProfileImage = styled.img`
+  width: 100%;
+`;
+
+export const BiographyContainer = styled.div``;
+
+export const MainBiographyWrapper = styled.div``;
+
+export const SecondaryBiographyWrapper = styled.div``;
