78adb623de0aa41775ca9b29d31b924089787f8b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-04 14:07:02 -0800

    feat(heading-typographies): create heading typographies for the h2 and h3 tags

---
+++ web/src/common/typography/HeadingSecondary/index.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+interface IHeadingSecondary {
+  letterSpacing?: number;
+  opacity?: number;
+}
+
+const HeadingSecondary = styled.h2<IHeadingSecondary>`
+  font-size: ${(props) => props.theme.fonts.headingSecondary};
+  font-weight: 700;
+  color: ${(props) => props.theme.colors.primary};
+
+  ${({ letterSpacing }) =>
+    letterSpacing && `letter-spacing: ${letterSpacing}rem`}
+
+  ${({ opacity }) => opacity && `opacity: ${opacity}`}
+`;
+
+export default HeadingSecondary;
