1c824a8ce7ad3c6ed9d78c5909683bc70e793109
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-04 12:46:27 -0800

    feat(hero-banner-styles): add additional styles

--- web/src/features/landing/components/HeroBanner/styles.ts
+++ web/src/features/landing/components/HeroBanner/styles.ts
@@ -6,15 +6,21 @@ export const Container = styled.div`
 `;
 
 export const ProfileImageWrapper = styled.div`
-  width: 7rem;
+  width: 15rem;
 `;
 
 export const ProfileImage = styled.img`
   width: 100%;
+  border-radius: 50%;
 `;
 
-export const BiographyContainer = styled.div``;
+export const BiographyContainer = styled.div`
+  width: 80%;
+  text-align: right;
+`;
 
-export const MainBiographyWrapper = styled.div``;
+export const MainBiographyWrapper = styled.div`
+  margin-bottom: 3rem;
+`;
 
 export const SecondaryBiographyWrapper = styled.div``;
