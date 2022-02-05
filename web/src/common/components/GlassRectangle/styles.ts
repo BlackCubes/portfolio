3295bbf97ccc521be70f71b434cb5b32d1af14d4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-05 13:29:30 -0800

    feat(glass-rectangle-styles): add interface and dynamically extend the box-shadow opacity from props

--- web/src/common/components/GlassRectangle/styles.ts
+++ web/src/common/components/GlassRectangle/styles.ts
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 
+interface IGlassContainer {
+  boxDarkShadowRgbaOpacity?: number;
+  boxLightShadowRgbaOpacity?: number;
+}
+
 interface IGlassImageWrapper {
   opacity?: number;
 }
 
-export const GlassContainer = styled.div`
+export const GlassContainer = styled.div<IGlassContainer>`
   display: flex;
   align-items: center;
   width: 15rem;
@@ -12,8 +17,12 @@ export const GlassContainer = styled.div`
   background-color: ${(props) => `rgba(${props.theme.colors.glass.rgb}, 0.17)`};
   border-radius: 2rem;
   box-shadow: ${(props) => `
-  -0.1rem -0.1rem 0 0 rgba(${props.theme.colors.glassLightShadow.rgb}, 0.17),
-  0.1rem 0.1rem 0 0 rgba(${props.theme.colors.glassDarkShadow.rgb}, 0.27)`};
+  -0.1rem -0.1rem 0 0 rgba(${props.theme.colors.glassLightShadow.rgb}, ${
+    props.boxLightShadowRgbaOpacity ?? '0.17'
+  }),
+  0.1rem 0.1rem 0 0 rgba(${props.theme.colors.glassDarkShadow.rgb}, ${
+    props.boxDarkShadowRgbaOpacity ?? '0.27'
+  })`};
 `;
 
 export const GlassImageWrapper = styled.div<IGlassImageWrapper>`
