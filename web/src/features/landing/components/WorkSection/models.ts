797d9018dcd83019251c7fb75cc630529ab4026d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-05 11:30:25 -0800

    feat(work-container-models): create interface for WorkContainer

---
+++ web/src/features/landing/components/WorkSection/models.ts
@@ -0,0 +1,8 @@
+export interface IWorkContainer {
+  workDescription: string;
+  workImageAlt: string;
+  workImageSrc: string;
+  workLinkContent: string;
+  workLinkPath: string;
+  workTitle: string;
+}
