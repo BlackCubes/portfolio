faa88f04e1b6aba34eecc6f2a7e510b4c4bddd91
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-05 12:00:32 -0800

    refactor(work-section): reduce the code and change it

--- web/src/features/landing/components/WorkSection/index.tsx
+++ web/src/features/landing/components/WorkSection/index.tsx
@@ -1,182 +1,67 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 
 import kovviImage from 'assets/img/kovvi.png';
 import pokedexAppImage from 'assets/img/pokedex-app.png';
 import toadTribuneImage from 'assets/img/toad-tribune.png';
 
-import GlassRectangle from 'common/components/GlassRectangle';
-
 import HeadingSecondary from 'common/typography/HeadingSecondary';
-import HeadingTertiary from 'common/typography/HeadingTertiary';
-import Paragraph from 'common/typography/Paragraph';
 
-import {
-  Container,
-  ProjectContainer,
-  ProjectDescription,
-  ProjectDescriptionContainer,
-  ProjectLink,
-  ProjectLinkWrapper,
-  ProjectTitle,
-  Section,
-  SectionTitle,
-} from './styles';
+import { IWorkContainer } from './models';
+import { Container, Section, SectionTitle } from './styles';
+import WorkContainer from './WorkContainer';
 
-const WorkSection: FC = () => {
-  const [isHover, setIsHover] = useState(false);
+const workContainerData: IWorkContainer[] = [
+  {
+    workDescription:
+      'A cross-platform mobile app built with React Native that allows users to search COVID-19 data in a region, and view vaccine trial data.',
+    workImageAlt: 'Kovvi',
+    workImageSrc: kovviImage,
+    workLinkContent: 'View',
+    workLinkPath: '#',
+    workTitle: 'Kovvie',
+  },
+  {
+    workDescription:
+      'A web app built with React and Node.js to provide top headline news from around the world: Politics, sports, stonks, movies, animals and your local weather.',
+    workImageAlt: 'The Toad Tribune',
+    workImageSrc: toadTribuneImage,
+    workLinkContent: 'View',
+    workLinkPath: '#',
+    workTitle: 'The Toad Tribune',
+  },
+  {
+    workDescription:
+      'A web app built with React that checks the type matchups of your Pokémon vs. any other Pokémon. With over 900 Pokémon in the Pokémon API database, the combinations are almost endless.',
+    workImageAlt: 'The Pokédex App',
+    workImageSrc: pokedexAppImage,
+    workLinkContent: 'View',
+    workLinkPath: '#',
+    workTitle: 'The Pokédex App',
+  },
+];
 
-  return (
-    <Section className="default-container">
-      <SectionTitle>
-        <HeadingSecondary letterSpacing={1.6} opacity={0.8}>
-          Work
-        </HeadingSecondary>
-      </SectionTitle>
+const WorkSection: FC = () => (
+  <Section className="default-container">
+    <SectionTitle>
+      <HeadingSecondary letterSpacing={1.6} opacity={0.8}>
+        Work
+      </HeadingSecondary>
+    </SectionTitle>
 
-      <Container>
-        <ProjectContainer
-          onMouseEnter={() => setIsHover(true)}
-          onMouseLeave={() => setIsHover(false)}
-        >
-          <ProjectTitle>
-            <HeadingTertiary
-              {...(isHover && {
-                opacity: 0.8,
-                textDecoration: 'underline',
-              })}
-            >
-              Kovvi
-            </HeadingTertiary>
-          </ProjectTitle>
-
-          <ProjectDescriptionContainer>
-            <ProjectDescription>
-              <Paragraph
-                {...(isHover && {
-                  opacity: 0.8,
-                })}
-              >
-                A cross-platform mobile app built with React Native that allows
-                users to search COVID-19 data in a region, and view vaccine
-                trial data.
-              </Paragraph>
-            </ProjectDescription>
-
-            <ProjectLinkWrapper>
-              <ProjectLink
-                href="#"
-                {...(isHover && {
-                  opacity: 0.7,
-                })}
-              >
-                View
-              </ProjectLink>
-            </ProjectLinkWrapper>
-          </ProjectDescriptionContainer>
-
-          <GlassRectangle
-            imageAlt="Kovvi"
-            imageSrc={kovviImage}
-            opacity={isHover ? 0.75 : 1}
-          />
-        </ProjectContainer>
-
-        <ProjectContainer
-          onMouseEnter={() => setIsHover(true)}
-          onMouseLeave={() => setIsHover(false)}
-        >
-          <ProjectTitle>
-            <HeadingTertiary
-              {...(isHover && {
-                opacity: 0.8,
-                textDecoration: 'underline',
-              })}
-            >
-              The Toad Tribune
-            </HeadingTertiary>
-          </ProjectTitle>
-
-          <ProjectDescriptionContainer>
-            <ProjectDescription>
-              <Paragraph
-                {...(isHover && {
-                  opacity: 0.8,
-                })}
-              >
-                A web app built with React and Node.js to provide top headline
-                news from around the world: Politics, sports, stonks, movies,
-                animals and your local weather.
-              </Paragraph>
-            </ProjectDescription>
-
-            <ProjectLinkWrapper>
-              <ProjectLink
-                href="#"
-                {...(isHover && {
-                  opacity: 0.7,
-                })}
-              >
-                View
-              </ProjectLink>
-            </ProjectLinkWrapper>
-          </ProjectDescriptionContainer>
-
-          <GlassRectangle
-            imageAlt="The Toad Tribune"
-            imageSrc={toadTribuneImage}
-            opacity={isHover ? 0.75 : 1}
-          />
-        </ProjectContainer>
-
-        <ProjectContainer
-          onMouseEnter={() => setIsHover(true)}
-          onMouseLeave={() => setIsHover(false)}
-        >
-          <ProjectTitle>
-            <HeadingTertiary
-              {...(isHover && {
-                opacity: 0.8,
-                textDecoration: 'underline',
-              })}
-            >
-              The Pokédex App
-            </HeadingTertiary>
-          </ProjectTitle>
-
-          <ProjectDescriptionContainer>
-            <ProjectDescription>
-              <Paragraph
-                {...(isHover && {
-                  opacity: 0.8,
-                })}
-              >
-                A web app built with React that checks the type matchups of your
-                Pokémon vs. any other Pokémon. With over 900 Pokémon in the
-                Pokémon API database, the combinations are almost endless.
-              </Paragraph>
-            </ProjectDescription>
-
-            <ProjectLinkWrapper>
-              <ProjectLink
-                href="#"
-                {...(isHover && {
-                  opacity: 0.7,
-                })}
-              >
-                View
-              </ProjectLink>
-            </ProjectLinkWrapper>
-          </ProjectDescriptionContainer>
-
-          <GlassRectangle
-            imageAlt="The Pokédex App"
-            imageSrc={pokedexAppImage}
-            opacity={isHover ? 0.75 : 1}
-          />
-        </ProjectContainer>
-      </Container>
-    </Section>
-  );
-};
+    <Container>
+      {workContainerData.map((workData) => (
+        <WorkContainer
+          key={workData.workTitle.toLowerCase().split(' ').join('-')}
+          workDescription={workData.workDescription}
+          workImageAlt={workData.workImageAlt}
+          workImageSrc={workData.workImageSrc}
+          workLinkContent={workData.workLinkContent}
+          workLinkPath={workData.workLinkPath}
+          workTitle={workData.workTitle}
+        />
+      ))}
+    </Container>
+  </Section>
+);
 
 export default WorkSection;
