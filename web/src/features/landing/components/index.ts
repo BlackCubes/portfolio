524d2df5ccdbbbe816be4654f62d02e7117397a3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-07 13:13:01 -0800

    feat(module-exports): export all components for landing page

---
+++ web/src/features/landing/components/index.ts
@@ -0,0 +1,4 @@
+export { default as ArticleSection } from './ArticleSection';
+export { default as HeroBanner } from './HeroBanner';
+export { default as TalkSection } from './TalkSection';
+export { default as WorkSection } from './WorkSection';
