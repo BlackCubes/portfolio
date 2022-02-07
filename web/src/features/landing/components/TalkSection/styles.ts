f9384f82c0969ea880086549e2a3f70889a3eda8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-06 18:55:38 -0800

    feat(talk-section-style): add dotted borders to SectionTitle

--- web/src/features/landing/components/TalkSection/styles.ts
+++ web/src/features/landing/components/TalkSection/styles.ts
@@ -4,7 +4,28 @@ import styled from 'styled-components';
 export const Section = styled.section``;
 
 export const SectionTitle = styled.div`
+  position: relative;
   text-align: center;
+
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-left: ${(props) =>
+      `0.1rem dotted rgba(${props.theme.colors.primary.rgb}, 0.7)`};
+  }
+
+  &:before {
+    bottom: 39rem;
+    left: 94rem;
+    transform: rotate(-45deg);
+  }
+
+  &:after {
+    left: 0;
+  }
 `;
 
 export const Container = styled.div`
