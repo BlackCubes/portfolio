543d1fb61c25843dc9a001fbbe5d31aabf75755b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-07 12:23:02 -0800

    feat(talk-section): render the talk container

--- web/src/features/landing/components/TalkSection/index.tsx
+++ web/src/features/landing/components/TalkSection/index.tsx
@@ -1,10 +1,31 @@
 import React, { FC } from 'react';
 
+import talkPresentation1 from 'assets/img/talk-presentation1.png';
+import talkPresentation2 from 'assets/img/talk-presentation2.png';
+
 import LineSeparator from 'common/components/LineSeparator';
 
 import HeadingSecondary from 'common/typography/HeadingSecondary';
 
 import { Container, Section, SectionTitle } from './styles';
+import TalkContainer, { ITalkContainer } from './TalkContainer';
+
+const talkContainerData: ITalkContainer[] = [
+  {
+    talkImageAlt: 'Django Part 1 Presentation',
+    talkImageSrc: talkPresentation1,
+    talkLinkPath: 'https://vimeo.com/659389997',
+    talkTitle:
+      '"Django Magic: MVT" Developer Connect Presentation @ Bitwise Industries',
+  },
+  {
+    talkImageAlt: 'Redux Toolkit Presentation',
+    talkImageSrc: talkPresentation2,
+    talkLinkPath: '#',
+    talkTitle:
+      '"Converting Legacy Redux To Redux Toolkit" Developer Connect Presentation @ Bitwise Industries',
+  },
+];
 
 const TalkSection: FC = () => (
   <Section className="default-container">
@@ -16,7 +37,20 @@ const TalkSection: FC = () => (
 
     <LineSeparator />
 
-    <Container>Hello</Container>
+    <Container>
+      {talkContainerData.map((talkData) => (
+        <React.Fragment
+          key={talkData.talkImageAlt.toLowerCase().split(' ').join('-')}
+        >
+          <TalkContainer
+            talkImageAlt={talkData.talkImageAlt}
+            talkImageSrc={talkData.talkImageSrc}
+            talkLinkPath={talkData.talkLinkPath}
+            talkTitle={talkData.talkTitle}
+          />
+        </React.Fragment>
+      ))}
+    </Container>
   </Section>
 );
 
