14e72f4fb573b6ca221a16a9e7a8c43cd03bc143
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-06 21:10:05 -0800

    feat(glass-triangle): add multiple triangles

--- web/src/common/components/GlassTriangle/index.tsx
+++ web/src/common/components/GlassTriangle/index.tsx
@@ -1,11 +1,21 @@
 import React, { FC } from 'react';
 
-import { GlassWrapper, GlassShadow } from './styles';
+import { GlassWrapper, GlassShadow, MultiGlass } from './styles';
 
 const GlassTriangle: FC = () => (
-  <GlassWrapper>
-    <GlassShadow />
-  </GlassWrapper>
+  <MultiGlass>
+    <GlassWrapper className="triangle1">
+      <GlassShadow />
+    </GlassWrapper>
+
+    <GlassWrapper className="triangle2 vertical-flip">
+      <GlassShadow />
+    </GlassWrapper>
+
+    <GlassWrapper className="triangle3">
+      <GlassShadow />
+    </GlassWrapper>
+  </MultiGlass>
 );
 
 export default GlassTriangle;
--- web/src/common/components/GlassTriangle/styles.ts
+++ web/src/common/components/GlassTriangle/styles.ts
@@ -1,13 +1,33 @@
 import styled from 'styled-components';
 
+export const MultiGlass = styled.div`
+  position: relative;
+  width: 35rem;
+  height: 20rem;
+`;
+
 export const GlassWrapper = styled.div`
+  position: absolute;
   width: 15rem;
   height: 15rem;
   filter: ${(props) =>
     `drop-shadow(-0.1rem -0.1rem 0rem rgba(${props.theme.colors.glassLightShadow.rgb}, 0.17)) 
     drop-shadow(0.1rem 0.1rem 0rem rgba(${props.theme.colors.glassDarkShadow.rgb}, 0.27))`};
 
-  .vertical-flip {
+  &.triangle1 {
+    top: 2rem;
+  }
+
+  &.triangle2 {
+    left: 6.5rem;
+  }
+
+  &.triangle3 {
+    top: -2rem;
+    left: 15rem;
+  }
+
+  &.vertical-flip {
     transform: scaleY(-1);
   }
 `;
