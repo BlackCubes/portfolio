b2f0bb8ca90c9d8499edee55d76a6360ff7bd307
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-07 13:42:48 -0800

    feat(router-dom): create parent route for landing page, and wrap BrowserRouter to the App component

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyle, theme } from 'common/base';
 import Footer from 'common/components/Footer';
 import Navbar from 'common/components/Navbar';
 
+import LandingRoutes from 'features/landing/Routes';
+
 const App = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
 
     <Navbar />
+
+    <Routes>
+      <Route path="/" element={<LandingRoutes />} />
+    </Routes>
 
     <Footer />
   </ThemeProvider>
