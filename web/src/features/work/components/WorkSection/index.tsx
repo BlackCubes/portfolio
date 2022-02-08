6b1c19259090c1ff325ed3a3040f7d91257089e5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-07 18:28:04 -0800

    feat(work-section): render the work container

--- web/src/features/work/components/WorkSection/index.tsx
+++ web/src/features/work/components/WorkSection/index.tsx
@@ -1,10 +1,42 @@
 import React, { FC } from 'react';
 
+import kovviImage from 'assets/img/kovvi.png';
+import pokedexAppImage from 'assets/img/pokedex-app.png';
+import toadTribuneImage from 'assets/img/toad-tribune.png';
+
 import LineSeparator from 'common/components/LineSeparator';
 
 import HeadingSecondary from 'common/typography/HeadingSecondary';
 
 import { Container, Section, SectionTitle } from './styles';
+import WorkContainer, { IWorkContainer } from './WorkContainer';
+
+const workContainerData: IWorkContainer[] = [
+  {
+    workDescription:
+      'A cross-platform mobile app built with React Native that allows users to search COVID-19 data in a region, and view vaccine trial data.',
+    workImageAlt: 'Kovvi',
+    workImageSrc: kovviImage,
+    workLinkPath: '/',
+    workTitle: 'Kovvie',
+  },
+  {
+    workDescription:
+      'A web app built with React and Node.js to provide top headline news from around the world: Politics, sports, stonks, movies, animals and your local weather.',
+    workImageAlt: 'The Toad Tribune',
+    workImageSrc: toadTribuneImage,
+    workLinkPath: '/',
+    workTitle: 'The Toad Tribune',
+  },
+  {
+    workDescription:
+      'A web app built with React that checks the type matchups of your Pokémon vs. any other Pokémon. With over 900 Pokémon in the Pokémon API database, the combinations are almost endless.',
+    workImageAlt: 'The Pokédex App',
+    workImageSrc: pokedexAppImage,
+    workLinkPath: '/',
+    workTitle: 'The Pokédex App',
+  },
+];
 
 const WorkSection: FC = () => (
   <Section className="default-container default-margin-bottom navbar-footer-space">
@@ -16,6 +48,17 @@ const WorkSection: FC = () => (
 
     <Container>
       <LineSeparator rotateClass="negative-rotate" />
+
+      {workContainerData.map((workData) => (
+        <WorkContainer
+          key={workData.workTitle.toLowerCase().split(' ').join('-')}
+          workDescription={workData.workDescription}
+          workImageAlt={workData.workImageAlt}
+          workImageSrc={workData.workImageSrc}
+          workLinkPath={workData.workLinkPath}
+          workTitle={workData.workTitle}
+        />
+      ))}
     </Container>
   </Section>
 );
