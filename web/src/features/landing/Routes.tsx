02879e817f5ad6e2569ef8afba80ccf2c8d3e9b6
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-09 16:23:24 -0800

    refactor(landing-page): change name of the component to match the role it does

--- web/src/features/landing/Routes.tsx
+++ web/src/features/landing/Routes.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import LandingPage from './pages';
+import LandingListView from './pages';
 
 const LandingRoutes: FC = () => (
   <Routes>
-    <Route path="" element={<LandingPage />} />
+    <Route path="" element={<LandingListView />} />
   </Routes>
 );
 
