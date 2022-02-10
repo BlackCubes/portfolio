654f3a4879ac5ae28de9336448ca741a46688778
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-09 16:04:06 -0800

    feat(work-page): create a page component

---
+++ web/src/features/work/pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+import { PortfolioSection, WorkSection } from 'features/work/components';
+
+const WorkPage: FC = () => (
+  <>
+    <WorkSection />
+
+    <PortfolioSection />
+  </>
+);
+
+export default WorkPage;
