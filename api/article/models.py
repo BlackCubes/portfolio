3bd6d2a21a9812a57bcaebcd758909e37ed67b48
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-10 23:18:57 -0800

    feat(article-models): add header image and a code block to ArticlePage

--- api/article/models.py
+++ api/article/models.py
@@ -4,7 +4,10 @@ from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core import blocks
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.images.blocks import ImageChooserBlock
+
+from wagtailcodeblock.blocks import CodeBlock
 
 
 class ImageWithCaptionBlock(blocks.StructBlock):
@@ -18,6 +21,9 @@ class ArticlePage(Page):
     """"""
 
     description = models.CharField(max_length=100)
+    header_image = models.ForeignKey(
+        "wagtailimages.Image", blank=True, null=True, on_delete=models.SET_NULL
+    )
     body = StreamField(
         [
             (
@@ -49,10 +55,12 @@ class ArticlePage(Page):
             ),
             ("image_with_caption", ImageWithCaptionBlock()),
             ("block_quote", blocks.BlockQuoteBlock(required=False)),
+            ("code", CodeBlock(label="Code")),
         ]
     )
 
     content_panels = Page.content_panels + [
         FieldPanel("description"),
+        ImageChooserPanel("header_image"),
         StreamFieldPanel("body"),
     ]
