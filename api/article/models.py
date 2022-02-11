5402b1ea44037df79738d35df0b54ef5f478f124
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-10 21:04:23 -0800

    feat(article-models): create a simple article page with wagtail

--- api/article/models.py
+++ api/article/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class ImageWithCaptionBlock(blocks.StructBlock):
+    """"""
+
+    image = ImageChooserBlock()
+    caption = blocks.CharBlock()
+
+
+class ArticlePage(Page):
+    """"""
+
+    description = models.CharField(max_length=100)
+    body = StreamField(
+        [
+            ("title", blocks.CharBlock()),
+            ("paragraph", blocks.RichTextBlock()),
+            ("image_with_caption", ImageWithCaptionBlock()),
+        ]
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+        FieldPanel("body"),
+    ]
