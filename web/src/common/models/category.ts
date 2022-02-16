1a3bf24a3e3bdb711d2c5a5924b066db6e6589e3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-15 16:23:45 -0800

    feat(category-response-models): create interfaces

---
+++ web/src/common/models/category.ts
@@ -0,0 +1,5 @@
+export interface Category {
+  id: number;
+  uuid: string;
+  name: string;
+}
---
+++ web/src/common/models/response.ts
@@ -0,0 +1,7 @@
+type MetaData = {
+  total_count: number;
+};
+
+export interface PaginationResponse {
+  meta: MetaData;
+}
