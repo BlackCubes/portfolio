859f826d82aa7a212a9ef15e46a6645af5ae82a8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-16 20:08:53 -0800

    feat(article-signals): add the complete layout for calculating the reading time

--- api/article/signals.py
+++ api/article/signals.py
@@ -9,8 +9,12 @@ def create_reading_time(sender, instance, **kwargs):
     """
     Creates a new reading time in seconds to the ``reading_time`` field.
 
-    Adds each second based on the character length. For inline images, according
-    to Medium, they add in 12 seconds and thus this function does the same.
+    Adds each character length for the total character length in the article.
+    For inline images, according to Medium, they add in 12 seconds. Once the
+    total character length is done, this gets calculated with the average
+    characters per minute an average adult reads to get minutes which is
+    converted into seconds which is added with the other seconds from inline
+    images to get the final reading time.
 
     NOTE:
 
@@ -21,6 +25,8 @@ def create_reading_time(sender, instance, **kwargs):
     if article:
         article_body = article.body
         total_article_character_length = 0
+        # Accoring to Medium, every inline image would add 12 seconds
+        inline_image_seconds = 0
 
         if article.title:
             total_article_character_length += len(article.title)
@@ -45,7 +51,7 @@ def create_reading_time(sender, instance, **kwargs):
                     "image" in stream_item.value
                     and stream_item.value["image"] is not None
                 ):
-                    total_article_character_length += 12
+                    inline_image_seconds += 12
 
             if stream_item.block.name == "code":
                 if (
@@ -60,4 +66,19 @@ def create_reading_time(sender, instance, **kwargs):
             ):
                 total_article_character_length += len(stream_item.value)
 
-        article.reading_time = total_article_character_length
+        # According to https://iovs.arvojournals.org/article.aspx?articleid=2166061
+        character_to_word_ratio = 664.5 / 153.5
+
+        # Average reading speed is 238 wpm according to
+        # https://digest.bps.org.uk/2019/06/13/most-comprehensive-review-to-date-suggests-the-average-persons-reading-speed-is-slower-than-commonly-thought/
+        average_reading_speed_wpm = 238
+
+        # Convert wpm to cpm (characters per minute) since `total_article_character_length`
+        # is in characters
+        average_reading_speed_cpm = average_reading_speed_wpm * character_to_word_ratio
+
+        total_time_seconds = (
+            total_article_character_length / average_reading_speed_cpm * 60
+        ) + inline_image_seconds
+
+        article.reading_time = total_time_seconds
