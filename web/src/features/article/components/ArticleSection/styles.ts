aee69b5c8b10ea203f825f733696e9b2986f56b6
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-17 13:27:16 -0800

    feat(article-section-styles): add styles for article section

--- web/src/features/article/components/ArticleSection/styles.ts
+++ web/src/features/article/components/ArticleSection/styles.ts
@@ -2,6 +2,18 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 // ARTICLE SECTION
+export const Section = styled.section``;
+
+export const SectionTitle = styled.div`
+  border-left: ${(props) =>
+    `0.1rem dotted rgba(${props.theme.colors.primary.rgb}, 0.7)`};
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 4rem;
+`;
 
 // ARTICLE CONTAINER
 export const ArticleContainerStyle = styled.div`
