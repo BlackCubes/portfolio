9de721d620f439d559cf57e5e80409d0e77ee6b7
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-17 18:13:25 -0800

    refactor(footer-navbar-links): add articles path

--- web/src/common/components/Footer/index.tsx
+++ web/src/common/components/Footer/index.tsx
@@ -25,7 +25,7 @@ const Footer: FC = () => (
         </Item>
 
         <Item>
-          <ItemLink to="/">Articles</ItemLink>
+          <ItemLink to="/articles">Articles</ItemLink>
         </Item>
 
         <Item>
