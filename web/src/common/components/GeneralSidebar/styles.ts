e225d5b5273e673a54d6006325c139ec06c765e2
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-23 15:46:52 -0800

    feat(sidebar-styles): add additional styles

--- web/src/common/components/GeneralSidebar/styles.ts
+++ web/src/common/components/GeneralSidebar/styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-export const Sidebar = styled.aside``;
+export const Sidebar = styled.aside`
+  position: fixed;
+  top: 27rem;
+  left: 24rem;
+`;
 
-export const SidebarContainer = styled.div``;
+export const SidebarContainer = styled.div`
+  width: 15rem;
+`;
--- web/src/features/article/components/FilterSidebar/styles.ts
+++ web/src/features/article/components/FilterSidebar/styles.ts
@@ -1,17 +1,45 @@
 import styled from 'styled-components';
 
-export const SidebarContainer = styled.div``;
+export const SidebarContainer = styled.div`
+  &:not(:last-child) {
+    margin-bottom: 3rem;
+  }
+`;
 
-export const SidebarTitle = styled.div``;
+export const SidebarTitle = styled.div`
+  margin-bottom: 1rem;
+
+  & h3 {
+    font-size: 2rem;
+  }
+`;
 
 export const SidebarList = styled.ul``;
 
-export const SidebarItem = styled.li``;
+export const SidebarItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
 
-export const FilterName = styled.span``;
+  &:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+`;
 
-export const FilterCheckbox = styled.div``;
+export const FilterName = styled.span`
+  font-size: 1.4rem;
+  text-transform: capitalize;
+`;
+
+export const FilterCheckbox = styled.div`
+  height: 1.4rem;
+  margin-left: 1rem;
+`;
 
 export const CheckboxInput = styled.input.attrs(() => ({
   type: 'checkbox',
-}))``;
+}))`
+  height: 100%;
+  cursor: pointer;
+`;
