d7a19ac83862fa74f228f37caaa475c111d26068
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-23 12:02:18 -0800

    feat(article-list-view): render the categories and tags sidebar with redux rtk

--- web/src/features/article/pages/ArticleListView.tsx
+++ web/src/features/article/pages/ArticleListView.tsx
@@ -1,13 +1,29 @@
 import React, { FC } from 'react';
 
 import { useGetArticlesQuery } from 'common/api/articleExtendedApi';
+import { useGetCategoriesQuery } from 'common/api/categoryExtendedApi';
+import { useGetTagsQuery } from 'common/api/tagExtendedApi';
 
-import { ArticleList } from 'features/article/components';
+import {
+  ArticleList,
+  CategorySidebar,
+  TagSidebar,
+} from 'features/article/components';
 
 const ArticleListView: FC = () => {
   const { data: articlesData } = useGetArticlesQuery();
+  const { data: categoriesData } = useGetCategoriesQuery();
+  const { data: tagsData } = useGetTagsQuery();
 
-  return <ArticleList articlesData={articlesData?.items ?? []} />;
+  return (
+    <>
+      <CategorySidebar categoriesData={categoriesData?.items ?? []} />
+
+      <TagSidebar tagsData={tagsData?.items ?? []} />
+
+      <ArticleList articlesData={articlesData?.items ?? []} />
+    </>
+  );
 };
 
 export default ArticleListView;
