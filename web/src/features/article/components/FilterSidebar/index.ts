378144482aef7a3b0b5d4923fb57ea87c9ec837f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-23 13:11:48 -0800

    workaround(module-exports): remove exports to refactor sidebar

--- web/src/features/article/components/FilterSidebar/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as CategorySidebar } from './CategorySidebar';
-export { default as TagSidebar } from './TagSidebar';
--- web/src/features/article/components/index.ts
+++ web/src/features/article/components/index.ts
@@ -1,3 +1,2 @@
 export { default as ArticleDetail } from './ArticleDetail';
 export { default as ArticleList } from './ArticleList';
-export { CategorySidebar, TagSidebar } from './FilterSidebar';
