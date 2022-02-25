dd29090f5c257a92cac9a7ea71f7ef4ca6d3f0af
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-24 16:28:26 -0800

    feat(related-sidebar): create component for related sidebar, styled it, and exported it

---
+++ web/src/features/article/components/RelatedSidebar/index.tsx
@@ -0,0 +1,83 @@
+import React, { FC } from 'react';
+
+import noImage from 'assets/img/no-image.png';
+
+import GeneralSidebar from 'common/components/GeneralSidebar';
+
+import GlassCircle from 'common/components/GlassCircle';
+
+import { IArticle } from 'common/models';
+
+import HeadingTertiary from 'common/typography/HeadingTertiary';
+
+import {
+  RelatedContainer,
+  RelatedImageWrapper,
+  RelatedItem,
+  RelatedLink,
+  RelatedTitle,
+  RelatedTitleWrapper,
+  SidebarContainer,
+  SidebarList,
+  SidebarTitle,
+} from './styles';
+
+type TRelatedArticle = Pick<
+  IArticle,
+  'header_image' | 'id' | 'title' | 'uuid'
+> & {
+  meta: Pick<IArticle['meta'], 'slug'>;
+};
+
+interface IRelatedSidebar {
+  relatedArticlesByCategory: TRelatedArticle[];
+}
+
+const RelatedSidebar: FC<IRelatedSidebar> = ({ relatedArticlesByCategory }) => (
+  <GeneralSidebar
+    sidebarClassName="related-sidebar"
+    sidebarContentElement={
+      <>
+        <SidebarTitle>
+          <HeadingTertiary>Related Categories</HeadingTertiary>
+        </SidebarTitle>
+
+        <SidebarContainer>
+          <SidebarList>
+            {relatedArticlesByCategory.map((relatedArticle) => (
+              <RelatedItem key={relatedArticle.uuid}>
+                <RelatedLink to={`/articles/${relatedArticle.id}`}>
+                  <RelatedContainer>
+                    <RelatedImageWrapper>
+                      <GlassCircle
+                        glassDarkShadowBlur={0.2}
+                        glassDarkShadowHorizontalOffset={0.1}
+                        glassDarkShadowVerticalOffset={0.1}
+                        glassLightShadowBlur={0.2}
+                        glassLightShadowHorizontalOffset={-0.1}
+                        glassLightShadowVerticalOffset={-0.1}
+                        imageAlt={relatedArticle.title}
+                        imageSrc={
+                          relatedArticle.header_image
+                            ? `http://localhost:8000${relatedArticle.header_image}`
+                            : noImage
+                        }
+                        opacity={1}
+                      />
+                    </RelatedImageWrapper>
+
+                    <RelatedTitleWrapper>
+                      <RelatedTitle>{relatedArticle.title}</RelatedTitle>
+                    </RelatedTitleWrapper>
+                  </RelatedContainer>
+                </RelatedLink>
+              </RelatedItem>
+            ))}
+          </SidebarList>
+        </SidebarContainer>
+      </>
+    }
+  />
+);
+
+export default RelatedSidebar;
---
+++ web/src/features/article/components/RelatedSidebar/styles.ts
@@ -0,0 +1,50 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+// GENERAL
+export const SidebarTitle = styled.div`
+  margin-bottom: 1rem;
+
+  & h3 {
+    font-size: 2rem;
+  }
+`;
+
+export const SidebarContainer = styled.div`
+  &:not(:last-child) {
+    margin-bottom: 3rem;
+  }
+`;
+
+export const SidebarList = styled.ul``;
+
+// RELATED
+export const RelatedItem = styled.li`
+  list-style: none;
+
+  &:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+`;
+
+export const RelatedLink = styled(Link)`
+  text-decoration: none;
+
+  &:hover h4 {
+    text-decoration: underline;
+  }
+`;
+
+export const RelatedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const RelatedImageWrapper = styled.div``;
+
+export const RelatedTitleWrapper = styled.div``;
+
+export const RelatedTitle = styled.h4`
+  font-size: 1.8rem;
+  color: ${(props) => props.theme.colors.primary.hex};
+`;
