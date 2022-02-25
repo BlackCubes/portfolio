ce15f2ba62bc1dffe69628842032e3613c3a4672
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-02-24 17:21:25 -0800

    feat(article-detail-view): render the related sidebar

--- web/src/features/article/pages/ArticleDetailView.tsx
+++ web/src/features/article/pages/ArticleDetailView.tsx
@@ -1,36 +1,73 @@
 import React, { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { useGetArticleByIdQuery } from 'common/api/articleExtendedApi';
+import {
+  useGetArticleByIdQuery,
+  useGetArticlesByRelatedCategoryQuery,
+} from 'common/api/articleExtendedApi';
 
-import { ArticleDetail } from 'features/article/components';
+import { ArticleDetail, RelatedSidebar } from 'features/article/components';
 
 type TArticleParams = {
   articleId: string;
 };
 
 const ArticleDetailView: FC = () => {
-  const [doNotInitiateQuery, setDoNotInitiateQuery] = useState<boolean>(true);
+  const [doNotInitiateArticleQuery, setDoNotInitiateArticleQuery] =
+    useState<boolean>(true);
+  const [
+    doNotInitiateRelatedArticlesQuery,
+    setDoNotInitiateRelatedArticlesQuery,
+  ] = useState<boolean>(true);
+
   const { articleId } = useParams<TArticleParams>();
+  const [categoryId, setCategoryId] = useState<number>(0);
 
   const { data: articleData } = useGetArticleByIdQuery(
     articleId ? parseInt(articleId, 10) : 0,
     {
-      skip: doNotInitiateQuery,
+      skip: doNotInitiateArticleQuery,
     }
   );
+  const { data: relatedArticlesByCategoryData } =
+    useGetArticlesByRelatedCategoryQuery(categoryId, {
+      skip: doNotInitiateRelatedArticlesQuery,
+    });
 
   useEffect(() => {
     const timer = setTimeout(() => {
       if (articleId) {
-        setDoNotInitiateQuery(false);
+        setDoNotInitiateArticleQuery(false);
       }
     }, 1000);
 
     return () => clearTimeout(timer);
   }, [articleId]);
 
-  return articleData ? <ArticleDetail articleData={articleData} /> : null;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (articleData) {
+        setCategoryId(articleData.categories[0].id);
+
+        setDoNotInitiateRelatedArticlesQuery(false);
+      }
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [articleData]);
+
+  return (
+    <>
+      {articleData ? <ArticleDetail articleData={articleData} /> : null}
+
+      {relatedArticlesByCategoryData?.items &&
+        relatedArticlesByCategoryData.items.length > 0 && (
+          <RelatedSidebar
+            relatedArticlesByCategoryData={relatedArticlesByCategoryData.items}
+          />
+        )}
+    </>
+  );
 };
 
 export default ArticleDetailView;
