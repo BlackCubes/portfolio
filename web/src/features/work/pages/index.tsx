af3788fe0a4ee740864fc64e28e0573b4639491a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-03-03 11:55:22 -0800

    feat(work-list-view): add SEO

--- web/src/features/work/pages/index.tsx
+++ web/src/features/work/pages/index.tsx
@@ -1,9 +1,84 @@
 import React, { FC } from 'react';
 
+import SEO from 'common/components/SEO';
+
 import { PortfolioSection, WorkSection } from 'features/work/components';
 
 const WorkListView: FC = () => (
   <>
+    <SEO
+      openGraphMetaTags={[
+        {
+          property: 'og:description',
+          content: 'Latest Work and Projects by Elias Gutierrez',
+        },
+        {
+          property: 'og:image',
+          content: `${window.location.origin}/website-preview.png`,
+        },
+        {
+          property: 'og:site_name',
+          content: "Elias Gutierrez's Portfolio",
+        },
+        {
+          property: 'og:title',
+          content:
+            'Work | Elias Gutierrez, Software Engineer & Full-Stack Web Developer',
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          property: 'og:url',
+          content: window.location.href,
+        },
+      ]}
+      primaryMetaTags={[
+        {
+          name: 'description',
+          content: 'Latest Work and Projects by Elias Gutierrez',
+        },
+        {
+          name: 'title',
+          content:
+            'Work | Elias Gutierrez, Software Engineer & Full-Stack Web Developer',
+        },
+      ]}
+      title="Work | Elias Gutierrez, Software Engineer & Full-Stack Web Developer"
+      twitterMetaTags={[
+        {
+          property: 'twitter:card',
+          content: 'summary',
+        },
+        {
+          property: 'twitter:creator',
+          content: '@_BlackCubes_',
+        },
+        {
+          property: 'twitter:description',
+          content: 'Latest Work and Projects by Elias Gutierrez',
+        },
+        {
+          property: 'twitter:image',
+          content: `${window.location.origin}/website-preview.png`,
+        },
+        {
+          property: 'twitter:site',
+          content: '@_BlackCubes_',
+        },
+        {
+          property: 'twitter:title',
+          content:
+            'Work | Elias Gutierrez, Software Engineer & Full-Stack Web Developer',
+        },
+        {
+          property: 'twitter:url',
+          content: window.location.href,
+        },
+      ]}
+    />
+
     <WorkSection />
 
     <PortfolioSection />
