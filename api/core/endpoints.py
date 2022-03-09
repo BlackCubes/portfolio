60d12145d7a710bc6d9f9e3d8989d6382424c7d8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-03-09 13:03:07 -0800

    feat(core-endpoints): create endpoint for work category

--- api/core/endpoints.py
+++ api/core/endpoints.py
@@ -5,13 +5,33 @@ from article.models import (
 
 from wagtail.api.v2.views import BaseAPIViewSet
 
+from work.models import WorkCategory
 
-class CategoriesAPIEndpoint(BaseAPIViewSet):
+
+class ArticleCategoriesAPIEndpoint(BaseAPIViewSet):
     """
-    Creates an API endpoint for categories.
+    Creates an API endpoint for categories in article.
     """
 
     model = ArticleCategory
+
+    body_fields = BaseAPIViewSet.body_fields + [
+        "uuid",
+        "name",
+    ]
+
+    listing_default_fields = BaseAPIViewSet.listing_default_fields + [
+        "uuid",
+        "name",
+    ]
+
+
+class WorkCategoriesAPIEndpoint(BaseAPIViewSet):
+    """
+    Creates an API endpoint for categories in work.
+    """
+
+    model = WorkCategory
 
     body_fields = BaseAPIViewSet.body_fields + [
         "uuid",
