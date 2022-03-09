beb72dacd7300922d4a708c7fa5ca8872f0c4476
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-03-09 12:58:34 -0800

    feat(work-models): create 'WorkCategory' and 'WorkPage' models, and autogenerate the slug

--- api/work/models.py
+++ api/work/models.py
@@ -1,3 +1,190 @@
+from django import forms
 from django.db import models
 
-# Create your models here.
+from core.utils import unique_slug_generator
+
+from uuid import uuid4
+
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    MultiFieldPanel,
+    StreamFieldPanel,
+)
+from wagtail.admin.forms.models import WagtailAdminModelForm
+from wagtail.api import APIField
+from wagtail.core.blocks import (
+    BlockQuoteBlock,
+    RichTextBlock,
+)
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.snippets.models import register_snippet
+
+from .blocks import ImageWithCaptionBlock
+from .fields import (
+    WorkBodySerializedField,
+    WorkCategorySerializedField,
+    WorkMainImageSerializedField,
+)
+
+
+@register_snippet
+class WorkCategory(models.Model):
+    """
+    A standard Django model with fields of ``uuid`` and ``name``.
+
+    Used for editing on the admin panel, and to add categories to a work.
+
+    NOTE:
+
+    This is used the same as ``ArticleCategory``, and instead of combining
+    both into one, I am keeping them separate so that this category is only
+    concerned about ``work`` while the other is concerned about ``category``.
+    """
+
+    uuid = models.UUIDField(unique=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=255)
+
+    panels = [
+        FieldPanel("name"),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "work categories"
+
+
+class WorkPage(Page):
+    """"""
+
+    uuid = models.UUIDField(unique=True, default=uuid4, editable=False)
+    description = models.CharField(max_length=200)
+    main_image = models.ForeignKey(
+        "wagtailimages.Image", blank=True, null=True, on_delete=models.SET_NULL
+    )
+    category = models.ForeignKey(
+        "work.WorkCategory", null=True, on_delete=models.SET_NULL
+    )
+    body = StreamField(
+        [
+            (
+                "paragraph",
+                RichTextBlock(
+                    features=[
+                        "h1",
+                        "h2",
+                        "h3",
+                        "h4",
+                        "h5",
+                        "h6",
+                        "bold",
+                        "italic",
+                        "ol",
+                        "ul",
+                        "hr",
+                        "link",
+                        "embed",
+                        "code",
+                        "superscript",
+                        "subscript",
+                        "strikethrough",
+                    ]
+                ),
+            ),
+            ("image_with_caption", ImageWithCaptionBlock()),
+            ("block_quote", BlockQuoteBlock()),
+        ]
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+        ImageChooserPanel("main_image"),
+        MultiFieldPanel(
+            [
+                FieldPanel("category", widget=forms.RadioSelect),
+            ],
+            heading="Work information",
+        ),
+        StreamFieldPanel("body"),
+    ]
+
+    api_fields = [
+        APIField("uuid"),
+        APIField("description"),
+        APIField("main_image", serializer=WorkMainImageSerializedField()),
+        APIField("category", serializer=WorkCategorySerializedField()),
+        APIField("body", serializer=WorkBodySerializedField()),
+    ]
+
+
+def clean(self):
+    """
+    A custom function to override the ``WagtailAdminModelForm`` ``clean``
+    function so that the ``slug`` field could be created uniquely if it exists
+    in the DB.
+    """
+
+    cleaned_data = super(WagtailAdminModelForm, self).clean()
+
+    if isinstance(self.instance, WorkPage):
+        title = cleaned_data.get("title", None)
+        slug = cleaned_data.get("slug", None)
+
+        if title:
+            cleaned_data["slug"] = unique_slug_generator(
+                instance=self.instance, new_slug=slug
+            )
+
+    return cleaned_data
+
+
+def full_clean(self, *args, **kwargs):
+    """
+    A custom function to override the ``WagtailAdminModelForm`` ``full_clean``
+    function so that the empty ``slug`` field could be created uniquely if it
+    exists in the DB.
+
+    This function works if a ``slug`` is not provided, and it removes any
+    potential errors from the field given from Wagtail since a unique ``slug``
+    would be generated in the end.
+
+    NOTE:
+
+    Strangely enough, the custom ``clean`` function above works when creating
+    a new work as it autogenerates the unique ``slug`` without any errors. It
+    also works when updating a work when there is a non-empty ``slug``.
+
+    Yet, the custom ``clean`` function breaks apart with errors from Wagtail
+    when updating a work with an empty ``slug`` even though it autogenerates.
+    This is where this custom function comes into play.
+    """
+    super(WagtailAdminModelForm, self).full_clean(*args, **kwargs)
+
+    if isinstance(self.instance, WorkPage):
+        if self.data:
+            title = None if not self.data["title"] else self.data["title"]
+            slug = None if not self.data["slug"] else self.data["slug"]
+
+            if title and not slug:
+                # Remember old state
+                _mutable = self.data._mutable
+
+                # Set to mutable
+                self.data._mutable = True
+
+                self.data["slug"] = unique_slug_generator(
+                    instance=self.instance, new_slug=slug
+                )
+
+                # Set mutable flag back
+                self.data._mutable = _mutable
+
+                if "slug" in self.errors:
+                    del self.errors["slug"]
+
+
+WagtailAdminModelForm.clean = clean
+WagtailAdminModelForm.full_clean = full_clean
