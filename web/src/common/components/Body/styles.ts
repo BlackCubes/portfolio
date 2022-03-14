71ca82c55c4b6801f4efb96e947d5a50e5e2edd3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-03-14 12:23:05 -0700

    feat(body-styles): style the h4, h5, and h6 tags

--- web/src/common/components/Body/styles.ts
+++ web/src/common/components/Body/styles.ts
@@ -13,6 +13,18 @@ export const BodyParagraph = styled.div`
     font-size: ${(props) => props.theme.fonts.headingTertiary};
   }
 
+  & h4 {
+    font-size: 2rem;
+  }
+
+  & h5 {
+    font-size: 1.5rem;
+  }
+
+  & h6 {
+    font-size: 1.2rem;
+  }
+
   & ul {
     list-style: none;
 
@@ -43,7 +55,10 @@ export const BodyParagraph = styled.div`
 
   & h1,
   & h2,
-  & h3 {
+  & h3,
+  & h4,
+  & h5,
+  & h6 {
     font-weight: 700;
     color: ${(props) => props.theme.colors.primary.hex};
   }
@@ -58,6 +73,9 @@ export const BodyParagraph = styled.div`
   & h1,
   & h2,
   & h3,
+  & h4,
+  & h5,
+  & h6,
   & p,
   & ul {
     margin-top: 2.5rem;
