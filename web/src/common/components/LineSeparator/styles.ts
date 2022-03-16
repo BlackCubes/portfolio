0980155edb9a4a45d5c2c6d5b045e848e3fce9ac
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-03-16 13:45:27 -0700

    refactor(line-separator-responsive): remove any stylings for max-width 899px

--- web/src/common/components/LineSeparator/styles.ts
+++ web/src/common/components/LineSeparator/styles.ts
@@ -12,12 +12,6 @@ export const Container = styled.div`
   &.negative-rotate {
     transform: rotate(-4deg);
   }
-
-  @media ${({ theme }) => theme.responsive.below899} {
-    display: block;
-    width: 100%;
-    margin-left: 0;
-  }
 `;
 
 export const PrimarySolidLine = styled.div`
@@ -37,8 +31,4 @@ export const SecondaryDottedLine = styled.div`
   width: 50%;
   border: ${(props) =>
     `0.1rem dotted rgba(${props.theme.colors.secondary.rgb}, 0.3)`};
-
-  @media ${({ theme }) => theme.responsive.below899} {
-    display: none;
-  }
 `;
