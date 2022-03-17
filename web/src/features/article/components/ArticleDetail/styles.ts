bf4a98fce074f94dd89ccf42a8985e51db3e5cc0
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-03-17 11:54:01 -0700

    feat(article-listpage-responsive): decrease 'max-width' and 'font-size' for max-width 599px

--- web/src/features/article/components/ArticleDetail/styles.ts
+++ web/src/features/article/components/ArticleDetail/styles.ts
@@ -10,6 +10,10 @@ export const Article = styled.article`
   @media ${({ theme }) => theme.responsive.below899} {
     max-width: 95%;
   }
+
+  @media ${({ theme }) => theme.responsive.below599} {
+    max-width: 93%;
+  }
 `;
 
 export const ArticleAdditionalInfo = styled.div`
@@ -116,6 +120,10 @@ export const ArticleDescription = styled.div`
 
     @media ${({ theme }) => theme.responsive.below899} {
       font-size: 1.75rem;
+    }
+
+    @media ${({ theme }) => theme.responsive.below599} {
+      font-size: 1.7rem;
     }
   }
 `;
