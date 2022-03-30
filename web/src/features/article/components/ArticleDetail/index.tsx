66d731625f6b5b030335db94701478595544892d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-03-30 12:28:17 -0700

    feat(article-detail): change the twitter icon style dynamically

--- web/src/features/article/components/ArticleDetail/index.tsx
+++ web/src/features/article/components/ArticleDetail/index.tsx
@@ -1,10 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import noImage from 'assets/img/no-image.png';
 import twitterIconBlack from 'assets/img/twitter-logo_black.png';
+import twitterIconWhite from 'assets/img/twitter-logo_white.png';
 
 import Body from 'common/components/Body';
 import GlassRectangle from 'common/components/GlassRectangle';
+
+import { useThemeContext } from 'common/contexts';
 
 import { IArticle } from 'common/models';
 
@@ -35,98 +38,112 @@ interface IArticleDetail {
   articleData: IArticle;
 }
 
-const ArticleDetail: FC<IArticleDetail> = ({ articleData }) => (
-  <Article>
-    <ArticleAdditionalInfo>
-      <ArticleCategory
-        {...(articleData.tags.length > 0 && {
-          className: 'has-tags',
-        })}
-      >
-        <Paragraph>{articleData.category?.name ?? ''}</Paragraph>
-      </ArticleCategory>
+const ArticleDetail: FC<IArticleDetail> = ({ articleData }) => {
+  const [twitterIcon, setTwitterIcon] = useState(twitterIconBlack);
 
-      {articleData.tags.length > 0 && (
-        <ArticleTags>
-          {articleData.tags.map((tag) => (
-            <Paragraph key={`${tag.slug}-${tag.id}`}>{tag.name}</Paragraph>
-          ))}
-        </ArticleTags>
-      )}
-    </ArticleAdditionalInfo>
+  const { isDark } = useThemeContext();
 
-    <ArticleTitle>
-      <HeadingPrimary>{articleData.title}</HeadingPrimary>
-    </ArticleTitle>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTwitterIcon(!isDark ? twitterIconBlack : twitterIconWhite);
+    }, 500);
 
-    <ArticleAuthor>
-      <Paragraph>Elias Gutierrez</Paragraph>
-    </ArticleAuthor>
+    return () => clearTimeout(timer);
+  }, [isDark]);
 
-    <AuthorTwitterContainer>
-      <AuthorTwitterIconWrapper>
-        <AuthorTwitterIcon
-          src={twitterIconBlack}
-          alt="Twitter Logo"
-          title="Follow Elias Gutierrez on Twitter"
+  return (
+    <Article>
+      <ArticleAdditionalInfo>
+        <ArticleCategory
+          {...(articleData.tags.length > 0 && {
+            className: 'has-tags',
+          })}
+        >
+          <Paragraph>{articleData.category?.name ?? ''}</Paragraph>
+        </ArticleCategory>
+
+        {articleData.tags.length > 0 && (
+          <ArticleTags>
+            {articleData.tags.map((tag) => (
+              <Paragraph key={`${tag.slug}-${tag.id}`}>{tag.name}</Paragraph>
+            ))}
+          </ArticleTags>
+        )}
+      </ArticleAdditionalInfo>
+
+      <ArticleTitle>
+        <HeadingPrimary>{articleData.title}</HeadingPrimary>
+      </ArticleTitle>
+
+      <ArticleAuthor>
+        <Paragraph>Elias Gutierrez</Paragraph>
+      </ArticleAuthor>
+
+      <AuthorTwitterContainer>
+        <AuthorTwitterIconWrapper>
+          <AuthorTwitterIcon
+            src={twitterIcon}
+            alt="Twitter Logo"
+            title="Follow Elias Gutierrez on Twitter"
+          />
+        </AuthorTwitterIconWrapper>
+
+        <AuthorTwitterLink
+          href="https://twitter.com/_BlackCubes_"
+          target="_blank"
+          rel="noopener"
+        >
+          @_BlackCubes_
+        </AuthorTwitterLink>
+      </AuthorTwitterContainer>
+
+      <ArticleAdditionalInfo className="article__date-readtime">
+        <ArticleDate>
+          <Paragraph>
+            {dateFormat('en-US', articleData.meta.first_published_at)}
+          </Paragraph>
+        </ArticleDate>
+
+        <ArticleReadTime>
+          <Paragraph>
+            {articleData.reading_time < 60
+              ? `${Math.round(articleData.reading_time)} sec`
+              : `${Math.floor(articleData.reading_time / 60)} min`}
+            &nbsp;read
+          </Paragraph>
+        </ArticleReadTime>
+      </ArticleAdditionalInfo>
+
+      <ArticleDescription>
+        <Paragraph>{articleData.description}</Paragraph>
+      </ArticleDescription>
+
+      <ArticleHeaderImage>
+        <GlassRectangle
+          customClassName="article-detail-page__header-image"
+          glassDarkShadowBlur={0.4}
+          glassDarkShadowHorizontalOffset={0.3}
+          glassDarkShadowVerticalOffset={0.3}
+          glassLightShadowBlur={0.4}
+          glassLightShadowHorizontalOffset={-0.3}
+          glassLightShadowVerticalOffset={-0.3}
+          imageAlt="Header image for article"
+          imageSrc={
+            articleData.header_image
+              ? `http://localhost:8000${articleData.header_image}`
+              : noImage
+          }
+          opacity={1}
         />
-      </AuthorTwitterIconWrapper>
+      </ArticleHeaderImage>
 
-      <AuthorTwitterLink
-        href="https://twitter.com/_BlackCubes_"
-        target="_blank"
-        rel="noopener"
-      >
-        @_BlackCubes_
-      </AuthorTwitterLink>
-    </AuthorTwitterContainer>
-
-    <ArticleAdditionalInfo className="article__date-readtime">
-      <ArticleDate>
-        <Paragraph>
-          {dateFormat('en-US', articleData.meta.first_published_at)}
-        </Paragraph>
-      </ArticleDate>
-
-      <ArticleReadTime>
-        <Paragraph>
-          {articleData.reading_time < 60
-            ? `${Math.round(articleData.reading_time)} sec`
-            : `${Math.floor(articleData.reading_time / 60)} min`}
-          &nbsp;read
-        </Paragraph>
-      </ArticleReadTime>
-    </ArticleAdditionalInfo>
-
-    <ArticleDescription>
-      <Paragraph>{articleData.description}</Paragraph>
-    </ArticleDescription>
-
-    <ArticleHeaderImage>
-      <GlassRectangle
-        customClassName="article-detail-page__header-image"
-        glassDarkShadowBlur={0.4}
-        glassDarkShadowHorizontalOffset={0.3}
-        glassDarkShadowVerticalOffset={0.3}
-        glassLightShadowBlur={0.4}
-        glassLightShadowHorizontalOffset={-0.3}
-        glassLightShadowVerticalOffset={-0.3}
-        imageAlt="Header image for article"
-        imageSrc={
-          articleData.header_image
-            ? `http://localhost:8000${articleData.header_image}`
-            : noImage
-        }
-        opacity={1}
-      />
-    </ArticleHeaderImage>
-
-    <ArticleBodyContainer>
-      {articleData.body.map((body) => (
-        <Body key={body.id} bodyType={body.type} bodyValue={body.value} />
-      ))}
-    </ArticleBodyContainer>
-  </Article>
-);
+      <ArticleBodyContainer>
+        {articleData.body.map((body) => (
+          <Body key={body.id} bodyType={body.type} bodyValue={body.value} />
+        ))}
+      </ArticleBodyContainer>
+    </Article>
+  );
+};
 
 export default ArticleDetail;
