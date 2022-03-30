6c9c10bb9fabd75a4368e26a9b4d32953867dd38
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-03-30 12:19:28 -0700

    feat(footer): change the logos style dynamically

--- web/src/common/components/Footer/index.tsx
+++ web/src/common/components/Footer/index.tsx
@@ -1,8 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
-import githubLogo from 'assets/img/github-logo_black.png';
-import linkedinLogo from 'assets/img/linkedin-logo_black.png';
-import twitterLogo from 'assets/img/twitter-logo_black.png';
+import githubLogoBlack from 'assets/img/github-logo_black.png';
+import linkedinLogoBlack from 'assets/img/linkedin-logo_black.png';
+import twitterLogoBlack from 'assets/img/twitter-logo_black.png';
+
+import githubLogoWhite from 'assets/img/github-logo_white.png';
+import linkedinLogoWhite from 'assets/img/linkedin-logo_white.png';
+import twitterLogoWhite from 'assets/img/twitter-logo_white.png';
+
+import { useThemeContext } from 'common/contexts';
 
 import { ItemLink, itemData } from '../ItemLink';
 
@@ -17,50 +23,70 @@ import {
   LogosContainer,
 } from './styles';
 
-const Footer: FC = () => (
-  <Foot className="default-container navbar-footer-space">
-    <Container>
-      <ItemsContainer>
-        {itemData.map((item) => (
-          <Item key={item.id}>
-            <ItemLink itemTitle={item.title} to={item.path} />
-          </Item>
-        ))}
-      </ItemsContainer>
+const Footer: FC = () => {
+  const [githubLogo, setGithubLogo] = useState(githubLogoBlack);
+  const [linkedinLogo, setLinkedinLogo] = useState(linkedinLogoBlack);
+  const [twitterLogo, setTwitterLogo] = useState(twitterLogoBlack);
 
-      <LogosContainer>
-        <LogoWrapper>
-          <LogoLink href="https://github.com/BlackCubes">
-            <Logo
-              src={githubLogo}
-              alt="Github Logo"
-              title="Follow Elias Gutierrez on GitHub"
-            />
-          </LogoLink>
-        </LogoWrapper>
+  const { isDark } = useThemeContext();
 
-        <LogoWrapper>
-          <LogoLink href="https://twitter.com/_BlackCubes_">
-            <Logo
-              src={twitterLogo}
-              alt="Twitter Logo"
-              title="Follow Elias Gutierrez on Twitter"
-            />
-          </LogoLink>
-        </LogoWrapper>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setGithubLogo(!isDark ? githubLogoBlack : githubLogoWhite);
 
-        <LogoWrapper>
-          <LogoLink href="https://www.linkedin.com/in/eliasgutierrez1991/">
-            <Logo
-              src={linkedinLogo}
-              alt="LinkedIn Logo"
-              title="Follow Elias Gutierrez on LinkedIn"
-            />
-          </LogoLink>
-        </LogoWrapper>
-      </LogosContainer>
-    </Container>
-  </Foot>
-);
+      setLinkedinLogo(!isDark ? linkedinLogoBlack : linkedinLogoWhite);
+
+      setTwitterLogo(!isDark ? twitterLogoBlack : twitterLogoWhite);
+    }, 600);
+
+    return () => clearTimeout(timer);
+  }, [isDark]);
+
+  return (
+    <Foot className="default-container navbar-footer-space">
+      <Container>
+        <ItemsContainer>
+          {itemData.map((item) => (
+            <Item key={item.id}>
+              <ItemLink itemTitle={item.title} to={item.path} />
+            </Item>
+          ))}
+        </ItemsContainer>
+
+        <LogosContainer>
+          <LogoWrapper>
+            <LogoLink href="https://github.com/BlackCubes">
+              <Logo
+                src={githubLogo}
+                alt="Github Logo"
+                title="Follow Elias Gutierrez on GitHub"
+              />
+            </LogoLink>
+          </LogoWrapper>
+
+          <LogoWrapper>
+            <LogoLink href="https://twitter.com/_BlackCubes_">
+              <Logo
+                src={twitterLogo}
+                alt="Twitter Logo"
+                title="Follow Elias Gutierrez on Twitter"
+              />
+            </LogoLink>
+          </LogoWrapper>
+
+          <LogoWrapper>
+            <LogoLink href="https://www.linkedin.com/in/eliasgutierrez1991/">
+              <Logo
+                src={linkedinLogo}
+                alt="LinkedIn Logo"
+                title="Follow Elias Gutierrez on LinkedIn"
+              />
+            </LogoLink>
+          </LogoWrapper>
+        </LogosContainer>
+      </Container>
+    </Foot>
+  );
+};
 
 export default Footer;
