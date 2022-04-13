caf745c12c9e4de889e9619b46f73f092632171e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-04-13 09:22:32 -0700

    refactor(loading-overlay-styles): move styled component to own file and rename its component

--- web/src/common/components/LoadingOverlay/index.tsx
+++ web/src/common/components/LoadingOverlay/index.tsx
@@ -1,29 +1,6 @@
 import React, { FC, useEffect, useState } from 'react';
-import styled from 'styled-components';
 
-const LoaderHeightStyled = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  margin-top: -30rem;
-  margin-bottom: -15rem;
-
-  @media ${({ theme }) => theme.responsive.below1199} {
-    margin-top: -20rem;
-  }
-
-  @media ${({ theme }) => theme.responsive.below899} {
-    margin-top: -10rem;
-  }
-
-  @media ${({ theme }) => theme.responsive.below599} {
-    margin-top: 0;
-    margin-bottom: -10rem;
-  }
-
-  @media ${({ theme }) => theme.responsive.between991And600} {
-    margin-top: -20rem;
-  }
-`;
+import { LoaderComponentWrapper } from './styles';
 
 interface ILoadingOverlay {
   contentComponent: JSX.Element | JSX.Element[];
@@ -49,7 +26,7 @@ const LoadingOverlay: FC<ILoadingOverlay> = ({
   }, [isLoading, loaderDuration]);
 
   return isLoadingFinal ? (
-    <LoaderHeightStyled>{loaderComponent}</LoaderHeightStyled>
+    <LoaderComponentWrapper>{loaderComponent}</LoaderComponentWrapper>
   ) : (
     /* eslint-disable-next-line react/jsx-no-useless-fragment */
     <>{contentComponent}</>
