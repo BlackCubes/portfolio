d50e8f740d0b4bcabf4b61e894c60dff372dbe82
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-04-13 13:59:46 -0700

    feat(site-transition): create a component for the landing page to have an initial transition

---
+++ web/src/features/landing/components/InitialSiteTransition/index.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from 'react';
+
+import {
+  Defs,
+  LinearGradient1,
+  LinearGradient2,
+  Polygon,
+  Stop,
+  Svg,
+  Wrapper,
+} from './styles';
+
+const InitialSiteTransition: FC = () => (
+  <Wrapper
+    animate="animate"
+    initial="initial"
+    variants={{
+      animate: {
+        height: 0,
+        transition: {
+          when: 'afterChildren',
+          duration: 2,
+          ease: [0.87, 0, 0.13, 1],
+        },
+      },
+      initial: { height: '100vh', bottom: 0 },
+    }}
+  >
+    <Svg>
+      <Defs>
+        <LinearGradient1>
+          <Stop offset="0" stopColor="#fff" />
+
+          <Stop offset="0.35" stopColor="#231f20" />
+
+          <Stop offset="1" stopColor="#000" />
+        </LinearGradient1>
+
+        <LinearGradient2>
+          <Stop offset="0" stopColor="#fff" />
+
+          <Stop offset="1" stopColor="#000" />
+        </LinearGradient2>
+      </Defs>
+
+      <Polygon />
+    </Svg>
+  </Wrapper>
+);
+
+export default InitialSiteTransition;
---
+++ web/src/features/landing/components/InitialSiteTransition/styles.ts
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const Wrapper = styled(motion.div)`
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.black.hex};
+  z-index: 50;
+`;
+
+export const Svg = styled(motion.svg).attrs(() => ({
+  id: 'Initial_Site_Transition_Layer_1',
+  xmlns: 'http://www.w3.org/2000/svg',
+  xmlnsXlink: 'http://www.w3.org/1999/xlink',
+  viewBox: '0 0 550 550',
+}))`
+  position: absolute;
+  transform: scale(0.2);
+  z-index: 50;
+
+  @media ${({ theme }) => theme.responsive.below1199} {
+    transform: scale(0.25);
+  }
+
+  @media ${({ theme }) => theme.responsive.below899} {
+    transform: scale(0.3);
+  }
+`;
+
+export const Defs = styled.defs``;
+
+export const Stop = styled.stop``;
+
+export const LinearGradient1 = styled.linearGradient.attrs(() => ({
+  gradientUnits: 'userSpaceOnUse',
+  id: 'initial-site-icon-transition-linear-gradient-1',
+  x1: '24',
+  x2: '506',
+  y1: '276',
+  y2: '276',
+}))``;
+
+export const LinearGradient2 = styled.linearGradient.attrs(() => ({
+  gradientUnits: 'userSpaceOnUse',
+  id: 'initial-site-icon-transition-linear-gradient-2',
+  x1: '23.98',
+  x2: '522',
+  y1: '276',
+  y2: '276',
+}))``;
+
+export const Polygon = styled(motion.polygon).attrs(() => ({
+  points:
+    '506 60.75 506 60.75 502 60.75 502 21.25 502 21.25 502 60.75 225.87 60.75 231.87 54.75 231.87 54.75 225.87 60.75 218 60.75 218 60.75 225.87 60.75 38.13 248.5 38.13 248.5 225.87 60.75 502 60.75 502 316.75 312 316.75 312 254.75 349.5 254.75 312 254.75 312 242.75 312 242.75 312 254.75 24.5 254.75 24.5 254.75 312 254.75 312 316.75 226 316.75 231.87 310.88 231.87 310.88 226 316.75 218 316.75 218 316.75 226 316.75 38.13 504.62 38.13 504.62 226 316.75 312 316.75 312 510.75 24 510.75 24 510.75 312 510.75 312 530.75 312 530.75 312 316.75 502 316.75 502 336.25 502 336.25 502 316.75 506 316.75 506 316.75 502 316.75 502 60.75 506 60.75',
+}))`
+  fill: url(#initial-site-icon-transition-linear-gradient-1);
+  stroke: url(#initial-site-icon-transition-linear-gradient-2);
+  stroke-miterlimit: 10;
+  stroke-width: 4rem;
+
+  @media ${({ theme }) => theme.responsive.below479} {
+    stroke-width: 3.5rem;
+  }
+
+  @media ${({ theme }) => theme.responsive.below379} {
+    stroke-width: 3.2rem;
+  }
+`;
