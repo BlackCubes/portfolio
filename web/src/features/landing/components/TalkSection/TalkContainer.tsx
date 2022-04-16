0f6c855a8f56f046335cf43c8457d2c3fa902c94
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-04-16 12:43:17 -0700

    refactor(talk-container): change the name of the variable to better fit what it targets

--- web/src/features/landing/components/TalkSection/TalkContainer.tsx
+++ web/src/features/landing/components/TalkSection/TalkContainer.tsx
@@ -38,7 +38,7 @@ const TalkContainer: FC<ITalkContainer> = ({
   const imageAnimateControls = useAnimation();
   const { inView: imageInView, ref: imageRef } = useInView();
 
-  const [isHovering, setIsHovering] = useIsHovering();
+  const [isTitleLinkHovering, setIsTitleLinkHovering] = useIsHovering();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -70,15 +70,17 @@ const TalkContainer: FC<ITalkContainer> = ({
         >
           <TalkImageLink href={talkLinkPath} target="_blank" rel="noopener">
             <GlassTriangle
-              glassDarkShadowBlur={isHovering ? 0.4 : 0}
-              glassDarkShadowHorizontalOffset={isHovering ? 0.3 : 0.1}
-              glassDarkShadowVerticalOffset={isHovering ? 0.3 : 0.1}
-              glassLightShadowBlur={isHovering ? 0.4 : 0}
-              glassLightShadowHorizontalOffset={isHovering ? -0.3 : -0.1}
-              glassLightShadowVerticalOffset={isHovering ? -0.3 : -0.1}
+              glassDarkShadowBlur={isTitleLinkHovering ? 0.4 : 0}
+              glassDarkShadowHorizontalOffset={isTitleLinkHovering ? 0.3 : 0.1}
+              glassDarkShadowVerticalOffset={isTitleLinkHovering ? 0.3 : 0.1}
+              glassLightShadowBlur={isTitleLinkHovering ? 0.4 : 0}
+              glassLightShadowHorizontalOffset={
+                isTitleLinkHovering ? -0.3 : -0.1
+              }
+              glassLightShadowVerticalOffset={isTitleLinkHovering ? -0.3 : -0.1}
               imageAlt={talkImageAlt}
               imageSrc={talkImageSrc}
-              opacity={isHovering ? 0.75 : 1}
+              opacity={isTitleLinkHovering ? 0.75 : 1}
               reverseClass={reverseClass}
             />
           </TalkImageLink>
@@ -89,11 +91,11 @@ const TalkContainer: FC<ITalkContainer> = ({
             href={talkLinkPath}
             target="_blank"
             rel="noopener"
-            onMouseEnter={() => setIsHovering(true)}
-            onMouseLeave={() => setIsHovering(false)}
+            onMouseEnter={() => setIsTitleLinkHovering(true)}
+            onMouseLeave={() => setIsTitleLinkHovering(false)}
           >
             <HeadingTertiary
-              {...(isHovering && {
+              {...(isTitleLinkHovering && {
                 opacity: 0.8,
                 textDecoration: 'underline',
               })}
