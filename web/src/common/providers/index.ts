fe56ecd6d63a2be40a5b4056dc9ef70ec0c79ba5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-04-18 13:38:23 -0700

    feat(providers): create custom provider

--- web/src/common/providers/index.ts
+++ web/src/common/providers/index.ts
@@ -1,2 +1,2 @@
-/* eslint-disable-next-line import/prefer-default-export */
+export { default as FirstMountProvider } from './FirstMountProvider';
 export { default as ThemeProvider } from './ThemeProvider';
