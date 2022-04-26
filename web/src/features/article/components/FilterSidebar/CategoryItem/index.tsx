65a228f6f641a21464b46513384a9cb620c11959
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-04-26 13:14:24 -0700

    feat(category-item): add animations

--- web/src/features/article/components/FilterSidebar/CategoryItem/index.tsx
+++ web/src/features/article/components/FilterSidebar/CategoryItem/index.tsx
@@ -1,21 +1,39 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { useAnimation } from 'framer-motion';
 
 import { CategoryItemContainer, CategoryName } from './styles';
 
 interface ICategoryItem {
   categoryName: string;
   handleCategoryOnClick: () => void;
+  isCategoryTitleBeingAnimated: boolean;
   isChecked: boolean;
 }
 
 const CategoryItem: FC<ICategoryItem> = ({
   categoryName,
   handleCategoryOnClick,
+  isCategoryTitleBeingAnimated,
   isChecked,
 }) => {
+  const nameAnimateControls = useAnimation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (isCategoryTitleBeingAnimated) {
+        nameAnimateControls.start('visible');
+      }
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [isCategoryTitleBeingAnimated, nameAnimateControls]);
+
   return (
     <CategoryItemContainer onClick={handleCategoryOnClick}>
-      <CategoryName className={isChecked ? 'checked' : ''}>
+      <CategoryName
+        animate={nameAnimateControls}
+        className={isChecked ? 'checked' : ''}
+      >
         {categoryName}
       </CategoryName>
     </CategoryItemContainer>
