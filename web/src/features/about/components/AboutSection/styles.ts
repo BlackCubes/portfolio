a71650ac219fc339d5d4aa03f787ccdbe06d2737
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-04-29 10:08:06 -0700

    feat(about-section): create a component for the about section in the about page

---
+++ web/src/features/about/components/AboutSection/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  width: 100%;
+  text-align: right;
+`;
+
+export const MainAboutWrapper = styled.div`
+  width: 80%;
+  margin-bottom: 3rem;
+
+  @media ${({ theme }) => theme.responsive.below899} {
+    width: 100%;
+    margin-bottom: 5rem;
+  }
+
+  @media ${({ theme }) => theme.responsive.below479} {
+    margin-bottom: 3rem;
+  }
+`;
+
+export const SecondaryAboutWrapper = styled.div`
+  width: 95%;
+`;
