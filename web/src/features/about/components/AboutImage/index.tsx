62e1efcb993108216ecd2f99d82ccc0b380fc872
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-04-29 13:47:35 -0700

    feat(about-image): create a component for the about image for the about page

---
+++ web/src/features/about/components/AboutImage/index.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+
+import aboutImage from 'assets/img/about-pic.jpg';
+
+import GlassRectangle from 'common/components/GlassRectangle';
+
+import { ImageWrapper } from './styles';
+
+const AboutImage: FC = () => (
+  <ImageWrapper>
+    <GlassRectangle
+      glassDarkShadowBlur={0.4}
+      glassDarkShadowHorizontalOffset={0.3}
+      glassDarkShadowVerticalOffset={0.3}
+      glassLightShadowBlur={0.4}
+      glassLightShadowHorizontalOffset={-0.3}
+      glassLightShadowVerticalOffset={-0.3}
+      imageAlt="Professional image of Elias Gutierrez"
+      imageSrc={aboutImage}
+      opacity={1}
+    />
+  </ImageWrapper>
+);
+
+export default AboutImage;
--- web/src/features/about/components/index.ts
+++ web/src/features/about/components/index.ts
@@ -1,3 +1,4 @@
+export { default as AboutImage } from './AboutImage';
 export { default as AboutSection } from './AboutSection';
 export { default as BeliefsSection } from './BeliefsSection';
 export { default as ExperienceSection } from './ExperienceSection';
