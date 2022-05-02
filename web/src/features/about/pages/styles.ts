851dc4d46e7104c85c563e0378cca0e5db647e47
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-02 11:02:31 -0700

    feat(about-pages-styles): increase width to 'Col' and change flex-direction to 'Row' for responsive max-width 899px

--- web/src/features/about/pages/styles.ts
+++ web/src/features/about/pages/styles.ts
@@ -30,8 +30,16 @@ export const PageContainer = styled(motion.div).attrs(() => ({
 export const Row = styled.div`
   display: flex;
   flex-direction: row;
+
+  @media ${({ theme }) => theme.responsive.below899} {
+    flex-direction: column;
+  }
 `;
 
 export const Col = styled.div`
   width: 50%;
+
+  @media ${({ theme }) => theme.responsive.below899} {
+    width: 100%;
+  }
 `;
