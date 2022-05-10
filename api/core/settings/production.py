03492469b51d6cfdfe130cc9a79df7f7bb32736b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-10 16:35:33 -0700

    feat(core-production): add extra settings to be production ready

--- api/core/settings/production.py
+++ api/core/settings/production.py
@@ -2,6 +2,31 @@ from .base import *
 
 DEBUG = False
 
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+ALLOWED_HOSTS = (
+    "BlackCubes.pythonanywhere",
+    "www.BlackCubes.pythonanywhere",
+)
+
+CORS_ALLOWED_ORIGINS = [
+    "https://eliastgutierrez.com",
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+]
+
+# HTTPS SETTINGS
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+# HSTS SETTINGS
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
 try:
     from .local import *
 except ImportError:
