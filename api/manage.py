9dd34a91c184d5fa68fd59e8f8825693758a9e68
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-10 15:52:43 -0700

    feat(base-manage): change the 'DJANGO_SETTINGS_MODULE' depending if it is in production or not

--- api/manage.py
+++ api/manage.py
@@ -10,7 +10,13 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 if __name__ == "__main__":
     load_dotenv()
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.dev")
+
+    DJANGO_SETTINGS_MODULE = "core.settings.dev"
+
+    if os.getenv("IN_PRODUCTION") == "yes":
+        DJANGO_SETTINGS_MODULE = "core.settings.production"
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", DJANGO_SETTINGS_MODULE)
 
     from django.core.management import execute_from_command_line
 
