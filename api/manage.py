44cc338595e41e31e08a0a1da7c73d31d1c25c3e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-10 14:45:35 -0700

    feat(base-manage): load the '.env' file to server (no security risk FYI)

--- api/manage.py
+++ api/manage.py
@@ -2,7 +2,14 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
+
+
 if __name__ == "__main__":
+    load_dotenv()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.dev")
 
     from django.core.management import execute_from_command_line
