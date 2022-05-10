35cd73b2605eaf71e58b6b8139348e27bb448f54
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-10 16:42:52 -0700

    feat(core-development): add the default SQLite database for development

--- api/core/settings/dev.py
+++ api/core/settings/dev.py
@@ -1,5 +1,15 @@
 from .base import *
 
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
