108d31172694658a17a15ab9ec9ef8dc8a9e5adb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-09 21:52:11 -0700

    refactor(environments): change API path

--- web/src/environment/environment.prod.ts
+++ web/src/environment/environment.prod.ts
@@ -3,7 +3,7 @@ import packageJson from '../../package.json';
 import { IEnvironment, EnvironmentConfiguration } from './types';
 
 const environment: IEnvironment = {
-  apiRoute: 'https://api.eliastgutierrez.com/api/v1',
+  apiRoute: 'https://api.eliastgutierrez.com',
   environment: EnvironmentConfiguration.Production,
   isProduction: process.env.NODE_ENV === 'production',
   name: packageJson.name,
