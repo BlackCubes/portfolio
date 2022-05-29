a69d24c03cd24b89363fab305424290f9e4535cb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-29 15:03:11 -0700

    style(pages): run prettier

--- new-web/pages/_app.tsx
+++ new-web/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 };
 
-export default MyApp
+export default MyApp;
