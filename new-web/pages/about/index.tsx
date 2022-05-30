1c9a554afd1c1a1e57f32a0cba03a9fc9dd7d810
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-30 16:03:52 -0700

    feat(about-pages): create a base level of component

---
+++ new-web/pages/about/index.tsx
@@ -0,0 +1,60 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const About: NextPage = () => (
+  <Head>
+    <title>
+      About | Elias Gutierrez, Software Engineer & Full-Stack Web Developer
+    </title>
+
+    <meta
+      name="title"
+      content="About | Elias Gutierrez, Software Engineer & Full-Stack Web Developer"
+    />
+
+    <meta
+      name="description"
+      content="Software Engineer and Full-Stack Web Developer. Architecting the art and mathemical model to create beautiful user experiences."
+    />
+
+    <meta property="og:site_name" content="Elias Gutierrez's Portfolio" />
+
+    <meta
+      property="og:title"
+      content="About | Elias Gutierrez, Software Engineer & Full-Stack Web Developer"
+    />
+
+    <meta
+      property="og:description"
+      content="Software Engineer and Full-Stack Web Developer. Architecting the art and mathemical model to create beautiful user experiences."
+    />
+
+    <meta property="og:image" content="" />
+
+    <meta property="og:type" content="website" />
+
+    <meta property="og:url" content="" />
+
+    <meta
+      property="twitter:title"
+      content="About | Elias Gutierrez, Software Engineer & Full-Stack Web Developer"
+    />
+
+    <meta
+      property="twitter:description"
+      content="Software Engineer and Full-Stack Web Developer. Architecting the art and mathemical model to create beautiful user experiences."
+    />
+
+    <meta property="twitter:image" content="" />
+
+    <meta property="twitter:card" content="summary" />
+
+    <meta property="twitter:creator" content="@_BlackCubes_" />
+
+    <meta property="twitter:site" content="@_BlackCubes" />
+
+    <meta property="twitter:url" content="" />
+  </Head>
+);
+
+export default About;
