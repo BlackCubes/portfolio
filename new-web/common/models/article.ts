ad717a1a3b190bfa4fadb077ec3c533d61f3035c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-29 21:14:39 -0700

    feat(models): create a model for the article

---
+++ new-web/common/models/article.ts
@@ -0,0 +1,66 @@
+import { ICategory } from './category';
+import { ITag } from './tag';
+
+type TMetaData = {
+  slug: string;
+  seo_title: string;
+  search_description: string;
+  first_published_at: string;
+};
+
+type TBodyParagraphData = {
+  id: string;
+  type: string;
+  value: string;
+};
+
+type TBodyImageWithCaptionData = {
+  id: string;
+  type: string;
+  value: {
+    image: string | null;
+    caption: string;
+  };
+};
+
+type TBodyBlockQuoteData = {
+  id: string;
+  type: string;
+  value: string;
+};
+
+type TBodyCodeData = {
+  id: string;
+  type: string;
+  value: {
+    language: string;
+    code: string;
+  };
+};
+
+type TBodyEquationData = {
+  id: string;
+  type: string;
+  value: string;
+};
+
+type TBodyData = Array<
+  | TBodyBlockQuoteData
+  | TBodyCodeData
+  | TBodyEquationData
+  | TBodyImageWithCaptionData
+  | TBodyParagraphData
+>;
+
+export interface IArticle {
+  id: number;
+  meta: TMetaData;
+  title: string;
+  uuid: string;
+  description: string;
+  header_image: string | null;
+  tags: ITag[] | [];
+  category: ICategory | null;
+  body: TBodyData;
+  reading_time: number;
+}
