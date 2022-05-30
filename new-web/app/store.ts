dd3eb341a0c9d29f5829e5efe356ca2e5b06d35e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-30 09:24:31 -0700

    feat(next-redux-wrapper): use 'next-redux-wrapper' to create server side rendering with Next.js via rehydration

--- new-web/app/store.ts
+++ new-web/app/store.ts
@@ -1,6 +1,9 @@
 import { configureStore, ConfigureStoreOptions } from '@reduxjs/toolkit';
+import { createWrapper } from 'next-redux-wrapper';
 
 import portfolioApi from 'common/api/coreSplitApi';
+
+import environment from 'environment';
 
 export const createAppStore = (
   options?: ConfigureStoreOptions['preloadedState'] | undefined
@@ -16,8 +19,12 @@ export const createAppStore = (
     ...options,
   });
 
-export const store = createAppStore();
+export type AppStore = ReturnType<typeof createAppStore>;
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<AppStore['getState']>;
 
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = AppStore['dispatch'];
+
+export const nextReduxWrapper = createWrapper<AppStore>(createAppStore, {
+  debug: !environment.isProduction,
+});
--- new-web/pages/_app.tsx
+++ new-web/pages/_app.tsx
@@ -1,15 +1,10 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
 
-import { store } from 'app';
+import { nextReduxWrapper } from 'app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+  return <Component {...pageProps} />;
 };
 
-export default MyApp;
+export default nextReduxWrapper.withRedux(MyApp);
