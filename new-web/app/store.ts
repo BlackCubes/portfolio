d0ddf2a6358c44c3a45ac96e98617dbb848dc04b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-29 23:31:38 -0700

    feat(redux-store): create a redux store

---
+++ new-web/app/store.ts
@@ -0,0 +1,23 @@
+import { configureStore, ConfigureStoreOptions } from '@reduxjs/toolkit';
+
+import portfolioApi from 'common/api/coreSplitApi';
+
+export const createAppStore = (
+  options?: ConfigureStoreOptions['preloadedState'] | undefined
+) =>
+  configureStore({
+    reducer: {
+      [portfolioApi.reducerPath]: portfolioApi.reducer,
+    },
+
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(portfolioApi.middleware),
+
+    ...options,
+  });
+
+export const store = createAppStore();
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type ApiDispatch = typeof store.dispatch;
