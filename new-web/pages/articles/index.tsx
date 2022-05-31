d090c7502490a5ca507c5065636a91f31afddef3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-30 18:27:56 -0700

    feat(articles-page): create a base component

---
+++ new-web/pages/articles/index.tsx
@@ -0,0 +1,57 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Articles: NextPage = () => (
+  <Head>
+    <title>
+      Articles | Elias Gutierrez, Software Engineer & Full-Stack Web Developer
+    </title>
+
+    <meta
+      name="title"
+      content="Articles | Elias Gutierrez, Software Engineer & Full-Stack Web Developer"
+    />
+
+    <meta name="description" content="Latest Articles by Elias Gutierrez" />
+
+    <meta property="og:site_name" content="Elias Gutierrez's Portfolio" />
+
+    <meta
+      property="og:title"
+      content="Articles | Elias Gutierrez, Software Engineer & Full-Stack Web Developer"
+    />
+
+    <meta
+      property="og:description"
+      content="Latest Articles by Elias Gutierrez"
+    />
+
+    <meta property="og:image" content="/website-preview.png" />
+
+    <meta property="og:type" content="website" />
+
+    <meta property="og:url" content="/" />
+
+    <meta
+      property="twitter:title"
+      content="Articles | Elias Gutierrez, Software Engineer & Full-Stack Web Developer"
+    />
+
+    <meta
+      property="twitter:description"
+      content="Latest Articles by Elias Gutierrez"
+    />
+
+    <meta property="twitter:image" content="/website-preview.png" />
+
+    <meta property="twitter:card" content="summary" />
+
+    <meta property="twitter:creator" content="@_BlackCubes_" />
+
+    <meta property="twitter:site" content="@_BlackCubes_" />
+
+    <meta property="twitter:url" content="/" />
+  </Head>
+);
+
+export default Articles;
