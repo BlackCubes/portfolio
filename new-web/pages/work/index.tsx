eb176af665f05516d4958c8f3a31213b516b6011
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-30 18:33:28 -0700

    feat(work-page): create a base component

---
+++ new-web/pages/work/index.tsx
@@ -0,0 +1,60 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Work: NextPage = () => (
+  <Head>
+    <title>
+      Work | Elias Gutierrez, Software Engineer & Full-Stack Web Developer
+    </title>
+
+    <meta
+      name="title"
+      content="Work | Elias Gutierrez, Software Engineer & Full-Stack Web Developer"
+    />
+
+    <meta
+      name="description"
+      content="Latest Work and Projects by Elias Gutierrez"
+    />
+
+    <meta property="og:site_name" content="Elias Gutierrez's Portfolio" />
+
+    <meta
+      property="og:title"
+      content="Work | Elias Gutierrez, Software Engineer & Full-Stack Web Developer"
+    />
+
+    <meta
+      property="og:description"
+      content="Latest Work and Projects by Elias Gutierrez"
+    />
+
+    <meta property="og:image" content="/website-preview.png" />
+
+    <meta property="og:type" content="website" />
+
+    <meta property="og:url" content="/" />
+
+    <meta
+      property="twitter:title"
+      content="Work | Elias Gutierrez, Software Engineer & Full-Stack Web Developer"
+    />
+
+    <meta
+      property="twitter:description"
+      content="Latest Work and Projects by Elias Gutierrez"
+    />
+
+    <meta property="twitter:image" content="/website-preview.png" />
+
+    <meta property="twitter:card" content="summary" />
+
+    <meta property="twitter:creator" content="@_BlackCubes_" />
+
+    <meta property="twitter:site" content="@_BlackCubes_" />
+
+    <meta property="twitter:url" content="/" />
+  </Head>
+);
+
+export default Work;
