99ce0c8c0dcc89111c2198b7f10d29768b1a635f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-30 21:46:15 -0700

    feat(app-pages): wrap custom theme provider

--- new-web/pages/_app.tsx
+++ new-web/pages/_app.tsx
@@ -3,8 +3,18 @@ import type { AppProps } from 'next/app';
 
 import { nextReduxWrapper } from 'app';
 
+import { GlobalStyle } from 'common/base';
+
+import { ThemeProvider } from 'common/providers';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <GlobalStyle />
+
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default nextReduxWrapper.withRedux(MyApp);
