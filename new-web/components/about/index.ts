a1a8f6d21cd83ff3389ae4c52f7bd9884d0ab8f1
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-01 12:09:45 -0700

    feat(row-col-components): create components with styles and module exports

--- new-web/components/about/index.ts
+++ new-web/components/about/index.ts
@@ -1,5 +1,7 @@
 export { default as AboutImage } from './AboutImage';
 export { default as AboutSection } from './AboutSection';
 export { default as BeliefsSection } from './BeliefsSection';
+export { default as Col } from './Col';
 export { default as ExperienceSection } from './ExperienceSection';
 export { default as MoreSection } from './MoreSection';
+export { default as Row } from './Row';
