9b3787ecde7f4a92c03a72e5ab3038e1d4c8f99c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-01 11:03:22 -0700

    feat(not-found): create component with styles

---
+++ new-web/common/components/NotFound/index.tsx
@@ -0,0 +1,53 @@
+import React, { FC, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+import Paragraph from 'common/typography/Paragraph';
+
+import {
+  Button,
+  Container,
+  DescriptionWrapper,
+  ImageStyled,
+  ImageWrapper,
+} from './styles';
+
+const NotFound: FC = () => {
+  const router = useRouter();
+
+  const containerAnimateControls = useAnimation();
+
+  const { inView: containerInView, ref: containerRef } = useInView();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (containerInView) {
+        containerAnimateControls.start('visible');
+      }
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, [containerAnimateControls, containerInView]);
+
+  return (
+    <Container animate={containerAnimateControls} ref={containerRef}>
+      <ImageWrapper>
+        <ImageStyled
+          alt="An image that shows the website logo and a 404 code wrapped in a comic bubble"
+          src="/not-found.png"
+        />
+      </ImageWrapper>
+
+      <DescriptionWrapper>
+        <Paragraph>
+          Something went wrong, or this page doesn&apos;t exist.
+        </Paragraph>
+      </DescriptionWrapper>
+
+      <Button onClick={() => router.back()}>Go back</Button>
+    </Container>
+  );
+};
+
+export default NotFound;
