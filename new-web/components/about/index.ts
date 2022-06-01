2acd04c96a09d75a969629ebfa6852725ec0fe67
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-01 11:47:58 -0700

    feat(more-section): create component with styles and module export

--- new-web/components/about/index.ts
+++ new-web/components/about/index.ts
@@ -2,3 +2,4 @@ export { default as AboutImage } from './AboutImage';
 export { default as AboutSection } from './AboutSection';
 export { default as BeliefsSection } from './BeliefsSection';
 export { default as ExperienceSection } from './ExperienceSection';
+export { default as MoreSection } from './MoreSection';
