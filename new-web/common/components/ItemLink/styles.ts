f8773123b225cf6a60f1b3c9c430f0eb7d5457f8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-31 23:20:55 -0700

    feat(item-link-styles): add additional styles

--- new-web/common/components/ItemLink/styles.ts
+++ new-web/common/components/ItemLink/styles.ts
@@ -7,14 +7,25 @@ export const ItemLinkStyle = styled.a`
   font-size: 2rem;
   color: ${({ theme }) => theme.colors.secondary.hex};
   padding: 0.7rem;
+  border-top: ${({ theme }) =>
+    `0.1rem solid rgba(${theme.colors.glassLightShadow.rgb}, 0)`};
+  border-right: ${({ theme }) =>
+    `0.1rem solid rgba(${theme.colors.glassDarkShadow.rgb}, 0)`};
+  border-bottom: ${({ theme }) =>
+    `0.1rem solid rgba(${theme.colors.glassDarkShadow.rgb}, 0)`};
+  border-left: ${({ theme }) =>
+    `0.1rem solid rgba(${theme.colors.glassLightShadow.rgb}, 0)`};
+  border-radius: 1rem;
   text-decoration: none;
   transition: all 0.05s linear;
+
+  &:hover {
+    cursor: pointer;
+  }
 
   &:hover,
   &.active {
     color: ${({ theme }) => `rgba(${theme.colors.secondary.rgb}, 0.6)`};
-    font-size: 1.89rem;
-    letter-spacing: 0.05rem;
     border-top: ${({ theme }) =>
       `0.1rem solid rgba(${theme.colors.glassLightShadow.rgb}, 0.17)`};
     border-right: ${({ theme }) =>
@@ -23,23 +34,6 @@ export const ItemLinkStyle = styled.a`
       `0.1rem solid rgba(${theme.colors.glassDarkShadow.rgb}, 0.27)`};
     border-left: ${({ theme }) =>
       `0.1rem solid rgba(${theme.colors.glassLightShadow.rgb}, 0.17)`};
-    border-radius: 1rem;
-
-    @media ${({ theme }) => theme.responsive.below899} {
-      font-size: 1.79rem;
-    }
-
-    @media ${({ theme }) => theme.responsive.below599} {
-      font-size: 1.59rem;
-    }
-
-    @media ${({ theme }) => theme.responsive.below479} {
-      font-size: 1.49rem;
-    }
-
-    @media ${({ theme }) => theme.responsive.below379} {
-      font-size: 1.39rem;
-    }
   }
 
   @media ${({ theme }) => theme.responsive.below899} {
