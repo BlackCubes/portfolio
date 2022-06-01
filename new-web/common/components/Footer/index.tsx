bd8b56e0975e81d4cd8f0aedefde9d189f6b26a2
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-05-31 23:51:05 -0700

    feat(footer): create component with styles

---
+++ new-web/common/components/Footer/index.tsx
@@ -0,0 +1,80 @@
+import React, { FC } from 'react';
+
+import { useThemeContext } from 'common/contexts';
+
+import { itemData, ItemLink } from '../ItemLink';
+
+import {
+  Container,
+  Foot,
+  Item,
+  ItemsContainer,
+  Logo,
+  LogoLink,
+  LogoWrapper,
+  LogosContainer,
+} from './styles';
+
+const Footer: FC = () => {
+  const { isDark } = useThemeContext();
+
+  return (
+    <Foot className="default-container navbar-footer-space">
+      <Container>
+        <ItemsContainer>
+          {itemData.map((item) => (
+            <Item key={item.id}>
+              <ItemLink itemTitle={item.title} href={item.path} />
+            </Item>
+          ))}
+        </ItemsContainer>
+
+        <LogosContainer>
+          <LogoWrapper>
+            <LogoLink
+              href="https://github.com/BlackCubes"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Logo
+                src={`github-logo_${isDark ? 'black' : 'white'}.png`}
+                alt="Github Logo"
+                title="Follow Elias Gutierrez on GitHub"
+              />
+            </LogoLink>
+          </LogoWrapper>
+
+          <LogoWrapper>
+            <LogoLink
+              href="https://twitter.com/_BlackCubes_"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Logo
+                src={`twitter-logo_${isDark ? 'black' : 'white'}.png`}
+                alt="Twitter Logo"
+                title="Follow Elias Gutierrez on Twitter"
+              />
+            </LogoLink>
+          </LogoWrapper>
+
+          <LogoWrapper>
+            <LogoLink
+              href="https://www.linkedin.com/in/eliasgutierrez1991/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <Logo
+                src={`linkedin-logo_${isDark ? 'black' : 'white'}.png`}
+                alt="LinkedIn Logo"
+                title="Follow Elias Gutierrez on LinkedIn"
+              />
+            </LogoLink>
+          </LogoWrapper>
+        </LogosContainer>
+      </Container>
+    </Foot>
+  );
+};
+
+export default Footer;
