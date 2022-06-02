cbc2a0953b8d6f092248f689f92785b2b3c87e49
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-02 16:49:38 -0700

    feat(talk-sections): create components with styles and module export

--- new-web/components/home/index.ts
+++ new-web/components/home/index.ts
@@ -1,3 +1,4 @@
 export { default as ArticleSection } from './ArticleSection';
 export { default as HeroBanner } from './HeroBanner';
 export { default as InitialSiteTransition } from './InitialSiteTransition';
+export { default as TalkSection } from './TalkSection';
