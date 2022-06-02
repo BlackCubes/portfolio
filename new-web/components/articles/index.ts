ed0f3fb8f9f2e2ac87626688544b4c1921dd2f06
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-01 20:56:20 -0700

    feat(article-pagination-container): create component with styles and module export

--- new-web/components/articles/index.ts
+++ new-web/components/articles/index.ts
@@ -1,5 +1,6 @@
 export { default as ArticleDetail } from './ArticleDetail';
 export { default as ArticleList } from './ArticleList';
+export { default as ArticleListPaginationContainer } from './ArticleListPaginationContainer';
 export { default as FilterSidebar } from './FilterSidebar';
 export { default as Pagination } from './Pagination';
 export { default as RelatedSidebar } from './RelatedSidebar';
