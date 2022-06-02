cb9dc5aa1fafbcb44c0197b277a7cee95ce906d7
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-02 15:26:01 -0700

    feat(custom-404-page): create custom 404 error page

---
+++ new-web/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import NotFound from 'common/components/NotFound';
+
+const Custom404 = () => (
+  <div className="default-container navbar-footer-space">
+    <NotFound />
+  </div>
+);
+
+export default Custom404;
