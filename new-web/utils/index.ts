137576fd647edf732cba2392ef0680be1b3be02f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-01 19:56:21 -0700

    feat(is-hovering-overall): create utility function

--- new-web/utils/index.ts
+++ new-web/utils/index.ts
@@ -1,2 +1,3 @@
 export * as darkThemeLocalStorage from './darkThemeLocalStorage';
 export { default as dateFormat } from './dateFormat';
+export { default as isHoveringOverall } from './isHoveringOverall';
