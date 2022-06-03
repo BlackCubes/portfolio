4dabe0b50e987e4ab26be4267a74dbcede60d01e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-03 16:12:39 -0700

    feat(next-config): add PWA

--- new-web/next.config.js
+++ new-web/next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+module.exports = withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+});
