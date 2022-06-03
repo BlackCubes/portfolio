b9d670f221d59d0fbd9d2cfc0378a45cc96f2299
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-02 19:00:39 -0700

    refactor(related-sidebar): remove components

--- new-web/components/articles/index.ts
+++ new-web/components/articles/index.ts
@@ -3,4 +3,3 @@ export { default as ArticleList } from './ArticleList';
 export { default as ArticleListPaginationContainer } from './ArticleListPaginationContainer';
 export { default as FilterSidebar } from './FilterSidebar';
 export { default as Pagination } from './Pagination';
-export { default as RelatedSidebar } from './RelatedSidebar';
