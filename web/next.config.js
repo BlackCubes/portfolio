b1ec2d092765b5861d689774c0395669c04b44be
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-04 02:43:38 -0700

    fix(manifest): add additional changes to remove manifest errors

--- web/next.config.js
+++ web/next.config.js
@@ -13,8 +13,12 @@ const initialNextConfig = {
 const nextConfig = withPWA({
   ...initialNextConfig,
   pwa: {
+    disable: process.env.NODE_ENV === 'development',
     dest: 'public',
+    register: true,
     runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+    skipWaiting: true,
   },
 });
 
