0df6f182fb70d4d0067b85599fe835c57145fe02
Elias Gutierrez <gutierrezelias1991@gmail.com>
2022-06-04 13:49:52 -0700

    refactor(styled-components-installation): remove any babelrc configs for styled-components but instead used the normal one presented by Next.js

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -26,13 +26,7 @@ export default class MyDocument extends Document {
 
       return {
         ...initialProps,
-        styles: [
-          <>
-            {initialProps.styles}
-
-            {sheet.getStyleElement()}
-          </>,
-        ],
+        styles: [initialProps.styles, sheet.getStyleElement()],
       };
     } finally {
       sheet.seal();
